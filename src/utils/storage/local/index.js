ea60906f6aee975baf25b8b26bb5e31d31530cdd
Pontus Abrahamsson <pontus@lostisland.co>
2018-06-17 22:17:20 +0200

    Add local and s3 storage

---
+++ src/utils/storage/local/index.js
@@ -0,0 +1,13 @@
+import { AsyncStorage } from 'react-native'
+
+const STORAGE_PREFIX = '@wrench:'
+
+export const setItem = (key, value) =>
+  AsyncStorage.setItem(STORAGE_PREFIX + key, JSON.stringify(value))
+
+export const getItem = async key => {
+  const item = await AsyncStorage.getItem(STORAGE_PREFIX + key)
+  return JSON.parse(item)
+}
+
+export const removeItem = key => AsyncStorage.removeItem(STORAGE_PREFIX + key)
