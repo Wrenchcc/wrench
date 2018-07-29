f6f5226066ae0eefc30958a5932d417438efc85c
Pontus Abrahamsson <pontus@lostisland.co>
2018-07-29 17:01:01 +0200

    Upgrade dependencies

--- src/ui/WebView/index.js
+++ src/ui/WebView/index.js
@@ -43,12 +43,13 @@ class WebView extends PureComponent {
   }
 
   onNavigationStateChange = navState => {
-    this.setState({
-      title: navState.title || this.state.title,
+    this.setState(prevState => ({
+      title: navState.title || prevState.title,
       canGoBack: navState.canGoBack,
       canGoForward: navState.canGoForward,
       url: navState.url,
-    })
+    }))
+
     if (this.isLoading) {
       this.increaseProgress()
     }
@@ -128,46 +129,46 @@ class WebView extends PureComponent {
     </Footer>
   )
 
-  render = () => console.log(this.setCustomHeaders()) || (
-      <Base>
-        <Header
-          transparent={false}
-          headerLeft={<Icon onPress={() => navigateBack()} source={closeDark} />}
-          headerCenter={
-            <View style={{ alignItems: 'center' }}>
-              <Text medium fontSize={15} numberOfLines={1} style={{ marginBottom: 3 }}>
-                {this.state.title}
-              </Text>
-              <Text fontSize={11} numberOfLines={1} color="light_grey">
-                {url.parse(this.state.url).host}
-              </Text>
-            </View>
-          }
-          headerRight={<Icon onPress={this.refresh} source={refresh} />}
-        />
-        <ProgressBar
-          opacity={this.isLoading ? 1 : 0}
-          fillColor="black"
-          borderRadius={0}
-          barHeight={2}
-          progress={this.progress}
-        />
+  render = () => (
+    <Base>
+      <Header
+        transparent={false}
+        headerLeft={<Icon onPress={() => navigateBack()} source={closeDark} />}
+        headerCenter={
+          <View style={{ alignItems: 'center' }}>
+            <Text medium fontSize={15} numberOfLines={1} style={{ marginBottom: 3 }}>
+              {this.state.title}
+            </Text>
+            <Text fontSize={11} numberOfLines={1} color="light_grey">
+              {url.parse(this.state.url).host}
+            </Text>
+          </View>
+        }
+        headerRight={<Icon onPress={this.refresh} source={refresh} />}
+      />
+      <ProgressBar
+        opacity={this.isLoading ? 1 : 0}
+        fillColor="black"
+        borderRadius={0}
+        barHeight={2}
+        progress={this.progress}
+      />
 
-        <RNWebView
-          style={{ flex: 1, backgroundColor: COLORS.LIGHT_GREY }}
-          source={{ uri: this.props.url, headers: this.setCustomHeaders() }}
-          javaScriptEnabled
-          onLoadStart={this.onLoadStart}
-          onLoadEnd={this.onLoadEnd}
-          onError={this.onLoadError}
-          onNavigationStateChange={this.onNavigationStateChange}
-          ref={ref => {
-            this.webview = ref
-          }}
-        />
+      <RNWebView
+        style={{ flex: 1, backgroundColor: COLORS.LIGHT_GREY }}
+        source={{ uri: this.props.url, headers: this.setCustomHeaders() }}
+        javaScriptEnabled
+        onLoadStart={this.onLoadStart}
+        onLoadEnd={this.onLoadEnd}
+        onError={this.onLoadError}
+        onNavigationStateChange={this.onNavigationStateChange}
+        ref={ref => {
+          this.webview = ref
+        }}
+      />
 
-        {this.renderFooter()}
-      </Base>
+      {this.renderFooter()}
+    </Base>
   )
 }
 
