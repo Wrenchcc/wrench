3e6ca42e1ed2ee0deaad498cd849b845df3f23c6
Pontus Abrahamsson <pontus@lostisland.co>
2018-08-25 18:52:22 +0200

    Add t to screenProps

--- src/navigation/AppNavigator/index.js
+++ src/navigation/AppNavigator/index.js
@@ -1,14 +1,16 @@
 import React, { PureComponent } from 'react'
 import { setNavigationRef } from 'navigation'
 import { Gateway, Zoomable } from 'ui'
+import { t } from 'i18n/withLocalization'
 import Navigator from './navigator'
+
 
 export default class TabNavigator extends PureComponent {
   render() {
     return (
       <Gateway.Provider>
         <Zoomable.Provider>
-          <Navigator ref={ref => setNavigationRef(ref)} />
+          <Navigator ref={ref => setNavigationRef(ref)} screenProps={{ t }} />
         </Zoomable.Provider>
       </Gateway.Provider>
     )
