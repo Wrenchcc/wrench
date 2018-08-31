53f7da51a38b8fae9f21b4a3fe4b2c280ab3db79
Pontus Abrahamsson <pontus@lostisland.co>
2018-08-31 15:27:32 +0200

    Use dynamic link

--- src/ui/Share/index.js
+++ src/ui/Share/index.js
@@ -1,34 +1,27 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import NativeShare from 'react-native-share'
-import { track, events, logError } from 'utils/analytics'
+import { track, events } from 'utils/analytics'
 import hitSlop from 'utils/hitSlop'
 import { share } from 'images'
-import { createDynamicLink } from 'utils/dynamicLinks'
 import { Base, Button, Icon } from './styles'
 
 export default class Share extends PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
   }
 
-  createLink = async () => {
-    const { path, title } = this.props
+  createLink = () => {
+    const { url, title } = this.props
     track(events.PROJECT_SHARE_OPEN)
 
-    try {
-      const url = await createDynamicLink({ path })
-
-      NativeShare.open({
-        title,
-        url,
-      }).catch(() => {
-        track(events.PROJECT_SHARE_CLOSED)
-      })
-    } catch (err) {
-      logError(err)
-    }
+    NativeShare.open({
+      title,
+      url,
+    }).catch(() => {
+      track(events.PROJECT_SHARE_CLOSED)
+    })
   }
 
   render() {
