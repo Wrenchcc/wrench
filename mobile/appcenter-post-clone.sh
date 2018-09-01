c8f03b5ab327ce3529555525735dde946bd6d3bf
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-02 00:26:49 +0200

    Disable librsvg

--- mobile/appcenter-post-clone.sh
+++ mobile/appcenter-post-clone.sh
@@ -6,6 +6,6 @@ NODE_VERSION="10.9.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
-echo "Install librsvg"
-HOMEBREW_NO_AUTO_UPDATE=1 brew install librsvg
-export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
+# echo "Install librsvg"
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install librsvg
+# export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
