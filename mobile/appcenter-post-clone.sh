150079b0a4c223605d4211b2d37cd4c8a443e61d
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-01 16:18:15 +0200

    Disable brew update

--- mobile/appcenter-post-clone.sh
+++ mobile/appcenter-post-clone.sh
@@ -6,5 +6,5 @@ NODE_VERSION="10.9.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
-echo "Install librsvg"
-HOMEBREW_NO_AUTO_UPDATE=1 brew install librsvg
+# echo "Install librsvg"
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install librsvg
