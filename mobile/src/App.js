1211dcd1e3993c16a39d6e722df4d545c0c9e66d
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-07 17:13:46 +0200

    Skip warnings

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -3,9 +3,13 @@ import { ApolloProvider, Query } from 'react-apollo'
 import { AuthNavigator, AppNavigator } from 'navigation'
 import { getCurrentUserQuery } from 'graphql/queries/user/getCurrentUser'
 import Onboarding from 'features/signIn/containers/Onboarding'
+import { YellowBox } from 'react-native'
 import { AppStateProvider, AppStateConsumer } from './AppState'
 
 if (__DEV__) {
+  // TODO: Remove
+  YellowBox.ignoreWarnings(['Require cycle:'])
+
   // Setting `global.XMLHttpRequest` will enable showing Network requests in React Native Debugger
   global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest
 }
