190f29a02a62f5c99aa6d85eda392ad887df7d2f
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-08 16:43:31 +0200

    Follow mutation

--- mobile/src/graphql/mutations/project/followProject.js
+++ mobile/src/graphql/mutations/project/followProject.js
@@ -17,6 +17,39 @@ const followProjectOptions = {
       variables: {
         id,
       },
+      // optimisticResponse: {
+      //   __typename: 'Mutation',
+      //   followProject: {
+      //     id: fakeId,
+      //     threadId: ownProps.thread.id,
+      //     type: 'like',
+      //     __typename: 'ThreadReaction',
+      //   },
+      // },
+      // update: (store, { data: { followProject } }) => {
+      //   const data = store.readQuery({
+      //     query: getThreadByIdQuery,
+      //     variables: {
+      //       id: ownProps.thread.id,
+      //     },
+      //   })
+      //
+      //   // ignore the server response and only update the cache with the
+      //   // optimistic response
+      //   if (typeof followProject.id === 'string') return
+      //
+      //   data.thread.reactions.count++
+      //   data.thread.reactions.hasReacted = true
+      //
+      //   // Write our data back to the cache.
+      //   store.writeQuery({
+      //     query: getThreadByIdQuery,
+      //     data,
+      //     variables: {
+      //       id: ownProps.thread.id,
+      //     },
+      //   })
+      // },
     }),
   }),
 }
