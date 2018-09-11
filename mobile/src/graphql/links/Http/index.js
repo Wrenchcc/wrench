015dd1ca263d564faeb6a30004524e8f762c24e3
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-11 15:46:35 +0200

    Add upload link

--- mobile/src/graphql/links/Http/index.js
+++ mobile/src/graphql/links/Http/index.js
@@ -1,6 +1,30 @@
 import Config from 'react-native-config'
+import { ApolloLink } from 'apollo-link'
 import { BatchHttpLink } from 'apollo-link-batch-http'
+import { createUploadLink } from 'apollo-upload-client'
 
-export default new BatchHttpLink({
+/**
+ * Apollo Terminating link
+ * The split will skip Batching if an
+ * operation's context contains hasUpload: true.
+ * Then within a mutation component, specify the context
+ * with hasUpload to activate the Upload link.
+ * https://github.com/jaydenseric/apollo-upload-client/issues/34#issuecomment-406157217
+ *
+ * const { data } = await uploadMutation({
+ * variables: { file },
+ *   context: {
+ *     hasUpload: true, // activate Upload link
+ *   }
+ * })
+ */
+
+const options = {
   uri: Config.WRENCH_GRAPHQL_URI,
-})
+}
+
+export default ApolloLink.split(
+  operation => operation.getContext().hasUpload,
+  createUploadLink(options),
+  new BatchHttpLink(options)
+)
