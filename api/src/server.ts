1552083478dd17e0c13524c4d6b011f7821a6f34
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2018-09-12 09:39:16 +0200

    Prettier

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,23 +1,23 @@
-import { ApolloServer } from "apollo-server";
-import resolvers from "./resolvers";
-import schema from "./schema";
-import models from "./models";
-import services from "./services";
-import { getUserFromRequest } from "./utils";
+import { ApolloServer } from 'apollo-server'
+import resolvers from './resolvers'
+import schema from './schema'
+import models from './models'
+import services from './services'
+import { getUserFromRequest } from './utils'
 
-const debug = require("debug")("api:server");
+const debug = require('debug')('api:server')
 
-const { PORT = 4000 } = process.env;
+const { PORT = 4000 } = process.env
 
 const server = new ApolloServer({
   schema,
   context: ({ req }) => ({
     user: getUserFromRequest(req),
     services,
-    models
-  })
-});
+    models,
+  }),
+})
 
 server.listen({ port: PORT }).then(({ url }) => {
-  debug("🚀  Server ready at %o", url);
-});
+  debug('🚀  Server ready at %o', url)
+})
