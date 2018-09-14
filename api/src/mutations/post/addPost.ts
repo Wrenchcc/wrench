eb32aa58710ad3eda9c62462d970affee7c6c31e
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-14 16:29:53 +0200

    Upload files resolver

--- api/src/mutations/post/addPost.ts
+++ api/src/mutations/post/addPost.ts
@@ -1,4 +1,32 @@
+import { createWriteStream } from 'fs'
+import { v4 } from 'uuid'
 import posts from '../../fixtures/posts'
+const debug = require('debug')('api:server')
+
+const uploadDir = './uploads'
+
+const processUpload = async upload => {
+  const { stream, mimetype, encoding } = await upload
+  return storeUpload({ stream })
+}
+
+const storeUpload = async ({ stream }): Promise<any> => {
+  const id = v4()
+  const path = `${uploadDir}/${id}.jpeg`
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on('finish', () => resolve({ id, path }))
+      .on('error', reject)
+  )
+}
 
 // TODO: Check if user data
-export default (_, args, ctx) => posts()[0].node
+export default async (_, { input }, ctx) => {
+  const data = await Promise.all(input.files.map(processUpload))
+
+  debug('%O', data)
+
+  return posts()[0].node
+}
--- api/src/types/Post.ts
+++ api/src/types/Post.ts
@@ -35,7 +35,7 @@ export default `
 
   input PostInput {
     projectId: ID!
-    caption: String!
+    caption: String
     files: [Upload]
   }
 
