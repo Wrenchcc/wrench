91057408985c35fe13027c5bbb37436281cb75fa
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-17 22:25:41 +0200

    api commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path')
+const debug = require('debug')('build:webpack')
+
+const dir = process.env.DIR
+
+if (!dir) throw new Error('Define directory to build with the -d option.')
+debug(`> Building ${dir}, entry: ${dir}/index.ts, output: ${dir}/dist/server.js`)
 
 module.exports = {
-  entry: './server.ts',
+  entry: `./${dir}/index.ts`,
   target: 'node',
   mode: 'production',
   stats: 'none',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, dir, 'dist'),
     filename: 'server.js',
     libraryTarget: 'commonjs',
   },
