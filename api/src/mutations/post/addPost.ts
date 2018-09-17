b103707a2407779dd9158c4bd2d1688dad0634ff
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-17 20:38:53 +0200

    Rename env keys

--- api/src/mutations/post/addPost.ts
+++ api/src/mutations/post/addPost.ts
@@ -3,15 +3,20 @@ import { v4 } from 'uuid'
 import posts from '../../fixtures/posts'
 const debug = require('debug')('api:server')
 
-const { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET, AWS_REGION } = process.env
+const {
+  API_AWS_ACCESS_KEY,
+  API_AWS_SECRET_ACCESS_KEY,
+  API_AWS_S3_BUCKET,
+  API_AWS_REGION,
+} = process.env
 
-const AWS_SIGNATURE_VERSION = 'v4'
+const API_AWS_SIGNATURE_VERSION = 'v4'
 
 const s3 = new S3({
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  region: AWS_REGION,
-  signatureVersion: AWS_SIGNATURE_VERSION,
+  accessKeyId: API_AWS_ACCESS_KEY,
+  secretAccessKey: API_AWS_SECRET_ACCESS_KEY,
+  region: API_AWS_REGION,
+  signatureVersion: API_AWS_SIGNATURE_VERSION,
 })
 
 const generateFileName = mimetype => {
@@ -33,7 +38,7 @@ const processUpload = async upload => {
   const res = await s3
     .upload({
       Key: fileName,
-      Bucket: AWS_S3_BUCKET,
+      Bucket: API_AWS_S3_BUCKET,
       Body: stream,
     })
     .promise()
