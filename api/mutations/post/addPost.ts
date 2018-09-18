b7c4fbd8a28e808eba1448e1f379aaa2df10823c
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2018-09-18 13:19:53 +0200

    Add uuid and shared

--- api/mutations/post/addPost.ts
+++ api/mutations/post/addPost.ts
@@ -1,59 +1,9 @@
-import { S3 } from 'aws-sdk'
-import { v4 } from 'uuid'
 import posts from '../../fixtures/posts'
 const debug = require('debug')('api:server')
 
-const {
-  APP_AWS_ACCESS_KEY,
-  APP_AWS_SECRET_ACCESS_KEY,
-  APP_AWS_S3_BUCKET,
-  APP_AWS_REGION,
-} = process.env
-
-const APP_AWS_SIGNATURE_VERSION = 'v4'
-
-const s3 = new S3({
-  accessKeyId: APP_AWS_ACCESS_KEY,
-  secretAccessKey: APP_AWS_SECRET_ACCESS_KEY,
-  region: APP_AWS_REGION,
-  signatureVersion: APP_AWS_SIGNATURE_VERSION,
-})
-
-const generateFileName = mimetype => {
-  const id = v4()
-
-  switch (mimetype) {
-    case 'image/jpeg':
-      return `${id}.jpeg`
-    default:
-      return
-  }
-}
-
-const processUpload = async upload => {
-  const { stream, mimetype } = await upload
-
-  const fileName = generateFileName(mimetype)
-
-  const res = await s3
-    .upload({
-      Key: fileName,
-      Bucket: APP_AWS_S3_BUCKET,
-      Body: stream,
-    })
-    .promise()
-
-  return {
-    uri: res.Location,
-    fileName,
-  }
-}
-
 // TODO: Check if user data
 export default async (_, { input }, ctx) => {
-  const data = await Promise.all(input.files.map(processUpload))
-
-  debug('%O', data)
+  debug('%O', input)
 
   return posts()[0].node
 }
--- jupiter/index.ts
+++ jupiter/index.ts
@@ -1,9 +1,50 @@
-import micro from 'micro'
+import micro, { json, send } from 'micro'
+import { S3 } from 'aws-sdk'
+import { v4 } from 'uuid'
 
 const debug = require('debug')('api:jupiter')
 
-const { PORT = 5000 } = process.env
+const {
+  PORT = 5000,
+  API_AWS_ACCESS_KEY,
+  API_AWS_SECRET_ACCESS_KEY,
+  API_AWS_S3_BUCKET,
+  API_AWS_REGION,
+} = process.env
 
-micro(async (req, res) => ({ data: 'ping' })).listen(PORT)
+const HOUR_IN_SECONDS = 3600
+
+const s3 = new S3({
+  accessKeyId: API_AWS_ACCESS_KEY,
+  secretAccessKey: API_AWS_SECRET_ACCESS_KEY,
+  region: API_AWS_REGION,
+  signatureVersion: 'v4',
+})
+
+export default micro(
+  async (req, res): Promise<{}> => {
+    const input = await json(req)
+
+    return Promise.all(
+      input.map(async ({ filename }) => {
+        const id = v4()
+
+        try {
+          const params = {
+            Bucket: API_AWS_S3_BUCKET,
+            Expires: HOUR_IN_SECONDS,
+            Key: id,
+          }
+
+          const url = await s3.getSignedUrl('putObject', params)
+
+          return { url, id }
+        } catch (err) {
+          debug('Error ‰O', err)
+        }
+      })
+    )
+  }
+).listen(PORT)
 
 debug('🚀  Jupiter ready at %s', `http://localhost:${PORT}`)
