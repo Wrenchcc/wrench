cea8121d96e6fc7c2835bcd41d61212559216e6e
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-20 18:36:31 +0200

    Crop single image

--- mobile/src/utils/image/cropImage.js
+++ mobile/src/utils/image/cropImage.js
@@ -2,21 +2,17 @@ import { ImageEditor } from 'react-native'
 
 const DEFAULT_SIZE = 2048
 
-export default async files => Promise.all(
-  files.map(
-    async ({ uri, width = DEFAULT_SIZE, height = DEFAULT_SIZE }) => new Promise(resolve => {
-      ImageEditor.cropImage(
-        uri,
-        {
-          offset: { x: 0, y: 0 },
-          size: { width, height },
-          resizeMode: 'contain',
-        },
-        uri => resolve(uri)
-      )
-    })
+export default async ({ uri, width = DEFAULT_SIZE, height = DEFAULT_SIZE }) => new Promise(resolve => {
+  ImageEditor.cropImage(
+    uri,
+    {
+      offset: { x: 0, y: 0 },
+      size: { width, height },
+      resizeMode: 'contain',
+    },
+    uri => resolve(uri)
   )
-)
+})
 
 // NOTE: Remove image from store when uploaded ImageStore.removeImageForTag()
 // Delete an image from the ImageStore. Images are stored in memory
