10153081475c37a03c6b4444fb3aff0576b5486d
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2018-09-21 10:49:39 +0200

    Use .nvmrc node version in build pipeline

--- mobile/appcenter-post-clone.sh
+++ mobile/appcenter-post-clone.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 set -ex
+version=`cat .nvmrc`
 brew uninstall node@6
-NODE_VERSION="10.11.0"
+NODE_VERSION="$version"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
