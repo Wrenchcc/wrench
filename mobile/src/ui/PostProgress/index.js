6b3fdadb55a86d3a34bc04016e1e8b67ac80ec8e
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-25 20:43:14 +0200

    PostProgress animation

--- mobile/src/ui/PostProgress/index.js
+++ mobile/src/ui/PostProgress/index.js
@@ -1,23 +1,38 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { Animated } from 'react-native'
 import { translate } from 'react-i18next'
 import { Text } from 'ui'
 import { Base, Cover, Content } from './styles'
 
+const HEIGHT = 60
+const DURATION = 150
+
 class PostProgress extends PureComponent {
+  animatedValue = new Animated.Value(0)
+
   static propTypes = {
     image: PropTypes.string,
     title: PropTypes.string,
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.handleToast(!nextProps.image)
+  }
+
+  handleToast(hide) {
+    Animated.timing(this.animatedValue, {
+      toValue: hide ? 0 : HEIGHT,
+      duration: DURATION,
+    }).start()
+  }
+
   render() {
     const { t, title, image } = this.props
 
-    if (!title || !image) return null
-
     return (
-      <Base>
-        <Cover source={{ uri: image }} />
+      <Base style={{ height: this.animatedValue }}>
+        {image && <Cover source={{ uri: image }} />}
 
         <Content>
           <Text numberOfLines={1}>{title}</Text>
--- mobile/src/ui/PostProgress/styles.js
+++ mobile/src/ui/PostProgress/styles.js
@@ -1,20 +1,14 @@
+import { Animated } from 'react-native'
 import styled from 'styled-components'
 import { COLORS } from 'ui/constants'
 
-export const Base = styled.View`
+export const Base = styled(Animated.View)`
   background-color: ${COLORS.DIVIDER};
-  position: absolute;
-  height: 60;
   width: 100%;
-  left: 0;
-  right: 0;
   flex-direction: row;
   align-items: center;
   padding-left: 20;
   padding-right: 20;
-  padding-top: 10;
-  padding-bottom: 10;
-  z-index: 100;
 `
 
 export const Cover = styled.Image`
