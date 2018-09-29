484f98ec477cc4bd4bd460d78af8a86ee4380c00
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-29 12:05:14 +0200

    Remove promise util and cropping on upload

--- mobile/src/utils/storage/s3/index.js
+++ mobile/src/utils/storage/s3/index.js
@@ -1,7 +1,5 @@
-import { cropImage } from 'utils/image'
 import { client } from 'graphql/createClient'
 import { preSignUrlsMutation } from 'graphql/mutations/upload/preSignUrls'
-import { runSequentially } from 'utils/Promise'
 import makeS3Request from './makeS3Request'
 
 // TODO: Figure out what data should be sent, new photos has only uri:s
@@ -9,16 +7,14 @@ export const upload = async files => {
   const uris = Object.keys(files)
   const filenames = uris.map(uri => ({ filename: files[uri].filename }))
 
-  // Return pre-signed urls
-  // Resize images and return uris
-  const [preSignedUrls, resizedImages] = await Promise.all([
-    client.mutate({ mutation: preSignUrlsMutation, variables: { input: filenames } }),
-    runSequentially(uris.map(uri => () => cropImage(uri))),
-  ])
+  const preSignedUrls = await client.mutate({
+    mutation: preSignUrlsMutation,
+    variables: { input: filenames },
+  })
 
   // Return filenames
   const result = await Promise.all(
-    resizedImages.map(async (uri, index) => {
+    uris.map(async (uri, index) => {
       const { url, type, filename } = preSignedUrls.data.preSignUrls[index]
       return makeS3Request(url, { uri: uri.uri, type, filename })
     })
