1f817b60c6c020734fc198a16bb5910facca5997
Pontus Abrahamsson <pontus@lostisland.co>
2018-09-30 16:27:45 +0200

    eslint and prettier

--- mobile/src/utils/image/cropImage.js
+++ mobile/src/utils/image/cropImage.js
@@ -2,15 +2,17 @@ import ImageManipulator from 'react-native-image-manipulator'
 
 const DEFAULT_SIZE = 2048
 
-// ImageManipulator.manipulate(
-//   'assets-library://asset/asset.HEIC?id=CC95F08C-88C3-4012-9D6D-64A413D254B3&ext=HEIC',
-//   [{ resize: { width: DEFAULT_SIZE, height: DEFAULT_SIZE } }],
-//   {
-//     compress: 0.8,
-//   }
-// ).then(e => console.log(e))
-
-// TODO: Fix scaling
-export default async uri => ImageManipulator.manipulate(uri, [{ resize: { width: DEFAULT_SIZE, height: DEFAULT_SIZE } }], {
-  compress: 0.8,
-})
+export default async uri => ImageManipulator.manipulate(
+  uri,
+  [
+    {
+      resize: {
+        width: DEFAULT_SIZE,
+        height: DEFAULT_SIZE,
+      },
+    },
+  ],
+  {
+    compress: 0.8,
+  }
+)
