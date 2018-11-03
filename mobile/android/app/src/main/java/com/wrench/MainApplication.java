cad7f28d53ead9beef84be9a21cdadde4a9fb05e
Pontus Abrahamsson <pontus@lostisland.co>
2018-11-03 09:51:56 +0100

    Add react native screens

--- mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -3,6 +3,7 @@ package com.wrench;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.microsoft.codepush.react.CodePush;
@@ -57,6 +58,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNScreensPackage(),
           new RNGestureHandlerPackage(),
           new RNFirebasePackage(),
           new RNFirebaseMessagingPackage(),
