3bc7d4e70e608fccfabf08a260d9e094083de7da
Pontus Abrahamsson <pontus@lostisland.co>
2018-11-14 20:59:30 +0100

    Add database

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server'
+import { createConnection } from 'typeorm'
 import { getUserFromRequest } from 'api/utils/auth'
 import schema from './schema'
 import models from './models'
@@ -8,16 +9,20 @@ const debug = require('debug')('api:server')
 
 const { PORT = 4000, NODE_ENV } = process.env
 
-const server = new ApolloServer({
-  context: ({ req }) => ({
-    models,
-    services,
-    user: getUserFromRequest(req),
-  }),
-  playground: NODE_ENV !== 'production',
-  schema,
-})
+createConnection()
+  .then(async connection => {
+    const server = new ApolloServer({
+      context: ({ req }) => ({
+        models,
+        services,
+        user: getUserFromRequest(req),
+      }),
+      playground: NODE_ENV !== 'production',
+      schema,
+    })
 
-server.listen({ port: PORT }).then(({ url }) => {
-  debug('🚀 Server ready at %s', url)
-})
+    server.listen({ port: PORT }).then(({ url }) => {
+      debug('🚀 Server ready at %s', url)
+    })
+  })
+  .catch(error => debug(error))
