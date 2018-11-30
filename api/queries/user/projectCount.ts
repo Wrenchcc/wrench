013c9d7c96aee31620d56352cb9392a94f5200dc
Pontus Abrahamsson <pontus@lostisland.co>
2018-11-30 14:07:22 +0100

    add projectCount

--- api/queries/user/projectCount.ts
+++ api/queries/user/projectCount.ts
@@ -1,7 +1,6 @@
 export default async ({ id }, _, ctx) => {
   try {
-    // return ctx.db.Project.getCountByUserId(id)
-    return 1
+    return ctx.db.Project.getCountByUserId(id)
   } catch (err) {
     console.log(err)
   }
