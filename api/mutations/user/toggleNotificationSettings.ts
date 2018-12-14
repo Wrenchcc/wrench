bacc97d200a692401ba5bbd77f4f0649c563dc5a
Pontus Abrahamsson <pontus@lostisland.co>
2018-12-15 00:21:10 +0100

    Remove debugging

--- api/mutations/user/toggleNotificationSettings.ts
+++ api/mutations/user/toggleNotificationSettings.ts
@@ -4,41 +4,35 @@ import { requireAuth } from 'api/utils/permissions'
 import { DEFAULT_NOTIFICATIONS, NOTIFICATIONS_COLUMN } from 'api/utils/notificationsTypes'
 
 export default requireAuth(async (_, args, ctx) => {
-  try {
-    const { notificationType } = args.input
+  const { notificationType } = args.input
 
-    if (!DEFAULT_NOTIFICATIONS.hasOwnProperty(notificationType)) {
-      console.log('Not a valid notificationType.')
-      return new UserInputError('Not a valid notificationType.')
-    }
-
-    const user = await ctx.db.User.findOne(ctx.userId)
-    console.log('user', user)
-
-    const prevSettings = await ctx.db.UserSettings.findOrCreate(
-      {
-        type: NOTIFICATIONS_COLUMN,
-        userId: ctx.userId,
-      },
-      {
-        type: NOTIFICATIONS_COLUMN,
-        user,
-        value: DEFAULT_NOTIFICATIONS,
-      }
-    )
-
-    // Update to new state
-    await ctx.db.UserSettings.update(prevSettings.id, {
-      value: {
-        ...DEFAULT_NOTIFICATIONS,
-        ...prevSettings.value,
-        [notificationType]: !prevSettings.value[notificationType],
-      },
-    })
-
-    console.log('prevSettings', prevSettings)
-    return user
-  } catch (err) {
-    console.log(err)
+  if (!DEFAULT_NOTIFICATIONS.hasOwnProperty(notificationType)) {
+    return new UserInputError('Not a valid notificationType.')
   }
+
+  const user = await ctx.db.User.findOne(ctx.userId)
+
+  // Get prev state
+  const prevSettings = await ctx.db.UserSettings.findOrCreate(
+    {
+      type: NOTIFICATIONS_COLUMN,
+      userId: ctx.userId,
+    },
+    {
+      type: NOTIFICATIONS_COLUMN,
+      user,
+      value: DEFAULT_NOTIFICATIONS,
+    }
+  )
+
+  // Update to new state
+  await ctx.db.UserSettings.update(prevSettings.id, {
+    value: {
+      ...DEFAULT_NOTIFICATIONS,
+      ...prevSettings.value,
+      [notificationType]: !prevSettings.value[notificationType],
+    },
+  })
+
+  return user
 })
