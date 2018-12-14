8965d061414c5a878b5aed2411d126da294d5295
Pontus Abrahamsson <pontus@lostisland.co>
2018-12-15 00:29:01 +0100

    Debug

--- api/mutations/user/toggleNotificationSettings.ts
+++ api/mutations/user/toggleNotificationSettings.ts
@@ -4,35 +4,39 @@ import { requireAuth } from 'api/utils/permissions'
 import { DEFAULT_NOTIFICATIONS, NOTIFICATIONS_COLUMN } from 'api/utils/notificationsTypes'
 
 export default requireAuth(async (_, args, ctx) => {
-  const { notificationType } = args.input
+  try {
+    const { notificationType } = args.input
 
-  if (!DEFAULT_NOTIFICATIONS.hasOwnProperty(notificationType)) {
-    return new UserInputError('Not a valid notificationType.')
-  }
-
-  const user = await ctx.db.User.findOne(ctx.userId)
-
-  // Get prev state
-  const prevSettings = await ctx.db.UserSettings.findOrCreate(
-    {
-      type: NOTIFICATIONS_COLUMN,
-      userId: ctx.userId,
-    },
-    {
-      type: NOTIFICATIONS_COLUMN,
-      user,
-      value: DEFAULT_NOTIFICATIONS,
+    if (!DEFAULT_NOTIFICATIONS.hasOwnProperty(notificationType)) {
+      return new UserInputError('Not a valid notificationType.')
     }
-  )
 
-  // Update to new state
-  await ctx.db.UserSettings.update(prevSettings.id, {
-    value: {
-      ...DEFAULT_NOTIFICATIONS,
-      ...prevSettings.value,
-      [notificationType]: !prevSettings.value[notificationType],
-    },
-  })
+    const user = await ctx.db.User.findOne(ctx.userId)
 
-  return user
+    // Get prev state
+    const prevSettings = await ctx.db.UserSettings.findOrCreate(
+      {
+        type: NOTIFICATIONS_COLUMN,
+        userId: ctx.userId,
+      },
+      {
+        type: NOTIFICATIONS_COLUMN,
+        user,
+        value: DEFAULT_NOTIFICATIONS,
+      }
+    )
+
+    // Update to new state
+    await ctx.db.UserSettings.update(prevSettings.id, {
+      value: {
+        ...DEFAULT_NOTIFICATIONS,
+        ...prevSettings.value,
+        [notificationType]: !prevSettings.value[notificationType],
+      },
+    })
+
+    return user
+  } catch (err) {
+    console.log(err)
+  }
 })
