04d594436ea66e8bd31d3b98eee4dbbf277a2546
Pontus Abrahamsson <pontus@lostisland.co>
2018-12-15 16:52:21 +0100

    Fix shadowing name

--- api/queries/user/projectCount.ts
+++ api/queries/user/projectCount.ts
@@ -1,6 +1,6 @@
 export default async ({ id }, _, ctx) => {
   try {
-    const [_, count] = await ctx.db.Project.findAndCount({ where: { userId: id } })
+    const [__, count] = await ctx.db.Project.findAndCount({ where: { userId: id } })
     return count
   } catch (err) {
     console.log(err)
