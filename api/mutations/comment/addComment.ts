8eb1ad6a7e521e126e0fc3f37e9466330b4e07a3
Pontus Abrahamsson <pontus@lostisland.co>
2018-12-23 11:22:13 +0100

    Update debug

--- api/mutations/comment/addComment.ts
+++ api/mutations/comment/addComment.ts
@@ -3,6 +3,8 @@ import { requireAuth } from 'api/utils/permissions'
 export default requireAuth(async (_, { postId, commentId, input }, ctx) => {
   const user = await ctx.db.User.findOne(ctx.userId)
 
+  // TODO: Parse text for mentions and send notifications
+
   return ctx.db.Comment.save({
     commentId,
     postId,
