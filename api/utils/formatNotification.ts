1769d5a01020c5943fcfaf36dda9e7a07ea1acbd
Pontus Abrahamsson <pontus@lostisland.co>
2018-12-26 23:11:24 +0100

    Remove titles from notifications

--- api/utils/formatNotification.ts
+++ api/utils/formatNotification.ts
@@ -6,17 +6,14 @@ export default function formatNotification(type, data, user) {
     case NOTIFICATION_TYPES.NEW_FOLLOWER:
       return {
         body: `${user.fullName} started following your project ${data.title}.`,
-        title: 'New follower',
       }
     case NOTIFICATION_TYPES.NEW_COMMENT:
       return {
         body: `${user.fullName} commented "${data.text}" on your project "${data.title}".`,
-        title: 'New comment',
       }
     case NOTIFICATION_TYPES.NEW_MENTION:
       return {
         body: `${user.fullName} mentioned you in a comment on the project "${data.title}".`,
-        title: 'Mention',
       }
     default:
       return null
