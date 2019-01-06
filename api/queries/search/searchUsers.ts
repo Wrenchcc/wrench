4b0c92c64defa7b03f7a488d701bb87d24196afe
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-06 11:24:24 +0100

    Formatting

--- api/queries/search/searchUsers.ts
+++ api/queries/search/searchUsers.ts
@@ -4,7 +4,9 @@ import paginate from 'api/utils/paginate'
 export default async (args, ctx) => {
   try {
     return paginate(ctx.db.User, args, {
-      where: { fullName: Raw(alias => `LOWER (${alias}) LIKE '%${args.query.toLowerCase()}%'`) },
+      where: {
+        fullName: Raw(alias => `LOWER (${alias}) LIKE '%${args.query.toLowerCase()}%'`),
+      },
     })
   } catch (err) {
     console.log(err)
