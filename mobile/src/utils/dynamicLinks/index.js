efd6cc9a501b27f931b47bdf47334ae2b2ef7829
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-06 20:18:04 +0100

    Fix dynamic link

--- mobile/src/utils/dynamicLinks/index.js
+++ mobile/src/utils/dynamicLinks/index.js
@@ -1,41 +1,14 @@
 import { Platform } from 'react-native'
 import Config from 'react-native-config'
-
-const getDomainName = url => {
-  let hostname
-  // find & remove protocol (http, ftp, etc.) and get hostname
-  if (url.indexOf('://') > -1) {
-    hostname = url.split('/')[2]
-  } else {
-    hostname = url.split('/')[0]
-  }
-  // find & remove port number
-  hostname = hostname.split(':')[0]
-  // find & remove "?"
-  hostname = hostname.split('?')[0]
-  hostname = hostname.replace('www.', '')
-
-  return hostname
-}
-
-Array.prototype.remove = (from, to) => {
-  const rest = this.slice((to || from) + 1 || this.length)
-  this.length = from < 0 ? this.length + from : from
-  return this.push.apply(this, rest)
-}
-
-export const extractDeepLinkFromDynamicLink = (dynamicLink = '') => {
-  const arr = dynamicLink.split('/').filter(str => str.length)
-  const hostname = getDomainName(dynamicLink)
-  arr.forEach((str, index) => {
-    if (str.indexOf('http') === 0 || str.indexOf('https') === 0 || str === hostname) {
-      arr.remove(index, index + 1)
-    }
-  })
-  if (Platform.OS === 'android') return `${hostname}://${hostname}/${arr.join('/')}`
-  return `${hostname}://${arr.join('/')}`
-}
+import url from 'url'
 
 export const uriPrefix = Platform.OS === 'android'
   ? `${Config.WRENCH_DEEP_LINK_BASE}wrench/`
   : Config.WRENCH_DEEP_LINK_BASE
+
+export const extractDeepLinkFromDynamicLink = (dynamicLink = '') => {
+  const { path } = url.parse(dynamicLink)
+
+  const uri = path.substring(1)
+  return `${uriPrefix}${uri}`
+}
