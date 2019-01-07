3c97f805492cfaea38a2acb584fd5f271b556a54
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-07 23:22:21 +0100

    Search models

--- api/queries/search/searchModels.ts
+++ api/queries/search/searchModels.ts
@@ -1,32 +1,43 @@
+import { ForbiddenError } from 'apollo-server-express'
 import { Raw, Like, getRepository } from 'typeorm'
 import paginate from 'api/utils/paginate'
 import Brand from 'api/models/Brand'
 
-// TODO: brand (name and year), sort by year
-// TODO: Max words to 3
-// https://github.com/typeorm/typeorm/issues/1322
-// https://github.com/typeorm/typeorm/issues/3119
 export default async (args, ctx) => {
-  const query = args.query.toLowerCase()
-  const words = query.split(' ')
+  if (!args.query) {
+    return new ForbiddenError('Please provide a search term.')
+  }
+
+  const words = args.query
+    .trim()
+    .toLowerCase()
+    .split(' ')
+
+  if (words.length > 5) {
+    return new ForbiddenError('Invalid search term.')
+  }
 
   const base = getRepository(Brand)
     .createQueryBuilder('brand')
     .select('*')
     .innerJoin('brand.models', 'models')
 
-  // userIds.forEach((userId, idx) => qb.orWhere('"userId" = :u'+idx, { ['u'+idx]: userId}));
-
-  words.forEach(word => {
+  words.forEach((word, i) => {
     const year = parseInt(word, 10)
     if (Number.isInteger(year)) {
       base.orWhere('models.year = :year', { year })
     } else {
       base
-        .orWhere('brand.name LIKE :word', { word: `%${word}%` })
-        .orWhere('models.model LIKE :word', { word: `%${word}%` })
+        .orWhere(`LOWER(brand.name) LIKE :word${i}`, { [`word${i}`]: `%${word}%` })
+        .orWhere(`LOWER(models.model) LIKE :word${i}`, { [`word${i}`]: `%${word}%` })
     }
   })
 
-  console.log(await base.getRawMany())
+  base.orderBy('models.year', 'DESC')
+
+  const edges = await base.getRawMany()
+
+  return {
+    edges: edges.map(node => ({ node })),
+  }
 }
