1f4b53008e56396fe93fe503cdfea667cddb02a4
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-07 22:30:31 +0100

    search model

--- api/queries/search/searchModels.ts
+++ api/queries/search/searchModels.ts
@@ -3,23 +3,28 @@ import paginate from 'api/utils/paginate'
 import Brand from 'api/models/Brand'
 
 // TODO: brand (name and year), sort by year
+// TODO: Max words to 3
+// https://github.com/typeorm/typeorm/issues/1322
+// https://github.com/typeorm/typeorm/issues/3119
 export default async (args, ctx) => {
   const query = args.query.toLowerCase()
   const words = query.split(' ')
 
-  let base = getRepository(Brand)
+  const base = getRepository(Brand)
     .createQueryBuilder('brand')
     .select('*')
     .innerJoin('brand.models', 'models')
 
+  // userIds.forEach((userId, idx) => qb.orWhere('"userId" = :u'+idx, { ['u'+idx]: userId}));
+
   words.forEach(word => {
     const year = parseInt(word, 10)
     if (Number.isInteger(year)) {
-      base = base.orWhere('models.year = :year', { year })
+      base.orWhere('models.year = :year', { year })
     } else {
-      base = base
+      base
         .orWhere('brand.name LIKE :word', { word: `%${word}%` })
-        .orWhere(`models.model LIKE :word`, { word: `%${word}%` }) // eslint-disable-line
+        .orWhere('models.model LIKE :word', { word: `%${word}%` })
     }
   })
 
