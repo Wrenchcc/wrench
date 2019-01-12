329facebb1f1fe8530a1a53f1242185221080e0b
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-12 10:12:39 +0100

    Search query

--- api/services/elasticsearch/utils/search.ts
+++ api/services/elasticsearch/utils/search.ts
@@ -2,7 +2,7 @@ import client from '../client'
 
 export default async query => {
   try {
-    return client.get('vehicles/_search?q=bmw')
+    return client.get(`vehicles/_search?q=${query}`)
   } catch (err) {
     console.log(err.response)
   }
