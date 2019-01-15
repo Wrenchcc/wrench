2190016fd449cecd8aca07cdaa811d568862197e
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-15 22:10:03 +0100

    Handle deeplinks and navigation params

--- mobile/src/navigation/utils/selectors.js
+++ mobile/src/navigation/utils/selectors.js
@@ -3,5 +3,18 @@ import { pathOr } from 'ramda'
 export const getProjectId = pathOr(null, ['state', 'params', 'project', 'id'])
 export const getPostId = pathOr(null, ['state', 'params', 'id'])
 export const getUserId = pathOr(null, ['state', 'params', 'user', 'id'])
-export const getUsername = pathOr(null, ['state', 'params', 'slug'])
-export const getProjectSlug = pathOr(null, ['state', 'params', 'slug'])
+
+// Support deeplink and navigation params
+export const getUsername = pathOr(pathOr(null, ['state', 'params', 'username']), [
+  'state',
+  'params',
+  'user',
+  'username',
+])
+
+export const getProjectSlug = pathOr(pathOr(null, ['state', 'params', 'slug']), [
+  'state',
+  'params',
+  'project',
+  'slug',
+])
