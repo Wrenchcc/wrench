34cc19fb415d8e219b515184bc4a194ef6cde81b
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-27 22:55:59 +0100

    Remove @babel/preset-typescript

--- packages/mobile/rn-cli.config.js
+++ packages/mobile/rn-cli.config.js
@@ -2,5 +2,13 @@ const path = require('path')
 
 // Allow files to imported from outside mobile root
 module.exports = {
-  getProjectRoots: () => [__dirname, path.join(__dirname, '..')],
+  getProjectRoots() {
+    return [
+      // Resolves the project directory.
+      path.resolve(__dirname),
+
+      // Resolves the monorepo's base folder
+      path.resolve(__dirname, '../..'),
+    ]
+  },
 }
