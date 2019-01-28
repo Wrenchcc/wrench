78a6677310733cc5b5a5f5019a5cd6a1d9cf42a2
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-01-28 10:55:47 +0100

    Enable playground

--- packages/api/src/utils/debugOptions.ts
+++ packages/api/src/utils/debugOptions.ts
@@ -1,8 +1,10 @@
 const { NODE_ENV } = process.env
 
-export default {
-  debug: true,
-  introspection: true,
-  playground: true,
-  tracing: true,
-}
+export default (true
+  ? {
+    debug: true,
+    introspection: true,
+    playground: true,
+    tracing: true,
+  }
+  : {})
