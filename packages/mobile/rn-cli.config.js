ef245a103d3e026e708fab2a67770d9935dd01b1
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-28 23:22:42 +0100

    WIP

--- packages/mobile/rn-cli.config.js
+++ packages/mobile/rn-cli.config.js
@@ -1,14 +1,12 @@
 const path = require('path')
-// https://github.com/facebook/react-native/issues/21922
+
 // Allow files to imported from outside mobile root
 module.exports = {
-  getProjectRoots() {
-    return [
-      // Resolves the project directory.
-      path.resolve(__dirname),
+  watchFolders: [
+    // Resolves the project directory.
+    path.resolve(__dirname),
 
-      // Resolves the monorepo's base folder
-      path.resolve(__dirname, '../..'),
-    ]
-  },
+    // Resolves the monorepo's base folder
+    path.resolve(__dirname, '../..'),
+  ],
 }
