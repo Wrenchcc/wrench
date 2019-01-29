46b020d797baece6c4cd446bc0275789d79baa66
Pontus Abrahamsson <pontus@lostisland.co>
2019-01-29 22:30:04 +0100

    Fix search api

--- packages/api/src/queries/search/searchProjects.ts
+++ packages/api/src/queries/search/searchProjects.ts
@@ -2,5 +2,7 @@ import { Raw } from 'typeorm'
 import paginate from '../../utils/paginate'
 
 export default async (args, ctx) => paginate(ctx.db.Project, args, {
-  where: { title: Raw(alias => `LOWER (${alias}) LIKE '%${args.query.toLowerCase()}%'`) },
+  where: {
+    title: Raw(alias => `LOWER (${alias}) LIKE '%${args.query.toLowerCase()}%'`),
+  },
 })
