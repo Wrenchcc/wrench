d53f8637f98309e2933e5cdb9c284350b3fdeb1c
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-01-29 17:03:45 +0100

    Add iOS assets

--- packages/mobile/rn-cli.config.js
+++ packages/mobile/rn-cli.config.js
@@ -10,3 +10,23 @@ module.exports = {
     // path.resolve(__dirname, '../../../'),
   ],
 }
+
+//
+// const path = require('path')
+// const blacklist = require('metro-config/src/defaults/blacklist');
+//
+// // Allow files to imported from outside mobile root
+// module.exports = {
+//   watchFolders: [
+//     // Resolves the project directory.
+//     path.resolve(__dirname),
+//
+//     // Resolves the monorepo's base folder
+//     path.resolve(__dirname, '../..'),
+//   ],
+//   resolver: {
+//     blacklistRE: blacklist([
+//       /node_modules\/.*\/node_modules\/react-native\/.*/,
+//     ])
+//   },
+// }
