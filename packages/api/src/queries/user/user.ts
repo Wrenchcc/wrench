2fda3a741ec99ef49b546657aef839717f12c06d
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-01-30 15:43:37 +0100

    Get user by username

--- packages/api/src/queries/user/user.ts
+++ packages/api/src/queries/user/user.ts
@@ -1,5 +1,5 @@
 // TODO: userByUsername loader
 export default async (_, args, ctx) => {
-  if (args.username) return ctx.loaders.userByUsername.load(args.username)
+  if (args.username) return ctx.db.User.findOne({ where: { username: args.username } })
   if (args.id) return ctx.loaders.user.load(args.id)
 }
