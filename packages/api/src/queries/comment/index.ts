b16b0b3ba75c1ae4f6d14d127bce2e518ffa7164
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-01 20:11:57 +0100

    Add commentId to reply

--- packages/api/src/queries/comment/index.ts
+++ packages/api/src/queries/comment/index.ts
@@ -1,9 +1,11 @@
+import commentId from './commentId'
 import comments from './comments'
 import repliesConnection from './repliesConnection'
 import user from './user'
 
 export default {
   Comment: {
+    commentId,
     repliesConnection,
     user,
   },
--- packages/api/src/types/Comment.ts
+++ packages/api/src/types/Comment.ts
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 export default gql`
   type Comment {
     id: ID
+    commentId: ID
     createdAt: Date
     updatedAt: Date
     text: String!
