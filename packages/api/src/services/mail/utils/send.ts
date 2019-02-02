160d4b8b9738a44159bd8e6a8931aa4d83e4fc70
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-02 17:43:02 +0100

    Add mailer

--- packages/api/src/services/mail/utils/send.ts
+++ packages/api/src/services/mail/utils/send.ts
@@ -2,7 +2,7 @@ import client from '../client'
 
 export default async ({ to = 'info@wdlinkoping.se', type = 'welcome', data = {} }) => {
   console.log(type, data)
-  
+
   try {
     const blah = await client.sendMail({
       from: 'pontus@wrench.cc',
