ed4da93b715d6db07e0c844f810902764acad835
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-12 21:32:40 +0100

    lock typeorm

--- packages/api/src/utils/paginate/findOperators.ts
+++ packages/api/src/utils/paginate/findOperators.ts
@@ -1,17 +1,17 @@
-import { LessThan, MoreThan } from 'typeorm'
+import { LessThanOrEqual, MoreThanOrEqual } from 'typeorm'
 import { decodeCursor } from './cursor'
 import { ORDER_BY } from './constants'
 
 export default ({ after, before }, { column, sort }) => {
   if (after) {
     const columnValue = decodeCursor(after)[1]
-    const comparator = sort === ORDER_BY.sort ? LessThan(columnValue) : MoreThan(columnValue)
+    const comparator = sort === ORDER_BY.sort ? LessThanOrEqual(columnValue) : MoreThanOrEqual(columnValue)
     return { [column]: comparator }
   }
 
   if (before) {
     const columnValue = decodeCursor(before)[1]
-    const comparator = sort === ORDER_BY.sort ? MoreThan(columnValue) : LessThan(columnValue)
+    const comparator = sort === ORDER_BY.sort ? MoreThanOrEqual(columnValue) : LessThanOrEqual(columnValue)
     return { [column]: comparator }
   }
 }
