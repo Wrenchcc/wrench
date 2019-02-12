ec34060107fe3b40a17443670c6fbf8c43e76b46
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-12 09:07:20 +0100

    Optimize image component

--- packages/mobile/src/ui/Image/index.js
+++ packages/mobile/src/ui/Image/index.js
@@ -1,62 +1,35 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-// import { Animated } from 'react-native'
 import { IMAGE_PRIORITY } from 'ui/constants'
 import { Base, FastImage } from './styles'
 
-// const FADE_DURATION = 50
+const Image = memo(function Image({
+  width,
+  height,
+  borderRadius,
+  placeholderColor,
+  priority,
+  ...props
+}) {
+  return (
+    <Base
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      placeholderColor={placeholderColor}
+    >
+      <FastImage {...props} priority={priority || IMAGE_PRIORITY.NORMAL} />
+    </Base>
+  )
+})
 
-export default class Image extends PureComponent {
-  static propTypes = {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    borderRadius: PropTypes.number,
-    placeholderColor: PropTypes.string,
-    priority: PropTypes.string,
-    disableAnimation: PropTypes.bool,
-  }
-
-  // state = {
-  //   opacity: new Animated.Value(0),
-  // }
-
-  // fadeIn = () => {
-  //   if (this.props.disableAnimation) return
-  //   Animated.spring(this.state.opacity, {
-  //     toValue: 1,
-  //     delay: 0,
-  //     duration: FADE_DURATION,
-  //     useNativeDriver: true,
-  //   }).start()
-  // }
-
-  // render = () => (
-  //   <Base
-  //     width={this.props.width}
-  //     height={this.props.height}
-  //     borderRadius={this.props.borderRadius}
-  //     placeholderColor={this.props.placeholderColor}
-  //   >
-  //     <Animated.View style={{ opacity: this.props.disableAnimation ? 1 : this.state.opacity }}>
-  //       <FastImage
-  //         {...this.props}
-  //         onLoadEnd={this.fadeIn}
-  //         priority={this.props.priority || IMAGE_PRIORITY.NORMAL}
-  //       />
-  //     </Animated.View>
-  //   </Base>
-  // )
-
-  render() {
-    return (
-      <Base
-        width={this.props.width}
-        height={this.props.height}
-        borderRadius={this.props.borderRadius}
-        placeholderColor={this.props.placeholderColor}
-      >
-        <FastImage {...this.props} priority={this.props.priority || IMAGE_PRIORITY.NORMAL} />
-      </Base>
-    )
-  }
+Image.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  borderRadius: PropTypes.number,
+  placeholderColor: PropTypes.string,
+  priority: PropTypes.string,
+  disableAnimation: PropTypes.bool,
 }
+
+export default Image
