c410b3e85490d1ad432bf57e14114e9bb677866c
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-14 15:19:27 +0100

    Add xhr to debugger

--- packages/mobile/src/App.js
+++ packages/mobile/src/App.js
@@ -11,6 +11,15 @@ import Onboarding from 'features/signIn/containers/Onboarding'
 
 useScreens()
 
+if (__DEV__) {
+  global.XMLHttpRequest = global.originalXMLHttpRequest
+    ? global.originalXMLHttpRequest
+    : global.XMLHttpRequest
+  global.FormData = global.originalFormData ? global.originalFormData : global.FormData
+  global.Blob = global.originalBlob ? global.originalBlob : global.Blob
+  global.FileReader = global.originalFileReader ? global.originalFileReader : global.FileReader
+}
+
 export default function App() {
   return (
     <Provider>
