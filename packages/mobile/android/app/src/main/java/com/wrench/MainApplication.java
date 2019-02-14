20d56081d34129124abb2293b97d45c3d9784903
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-14 19:50:41 +0100

    Update android

--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -3,54 +3,17 @@ package com.wrench;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.microsoft.codepush.react.CodePush;
-import com.mkuczera.RNReactNativeHapticFeedbackPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.dylanvann.fastimage.FastImageViewPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import cl.json.RNSharePackage;
-import com.brentvatne.react.ReactVideoPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
-import io.invertase.firebase.links.RNFirebaseLinksPackage;
-import io.invertase.firebase.perf.RNFirebasePerformancePackage;
-import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
-
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-    @Override
-    protected String getJSBundleFile() {
-      return CodePush.getJSBundleFile();
-    }
-
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -59,28 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new ReanimatedPackage(),
-          new RNScreensPackage(),
-          new RNCWebViewPackage(),
-          new RNGestureHandlerPackage(),
-          new RNFirebasePackage(),
-          new RNFirebaseMessagingPackage(),
-          new RNFirebaseAnalyticsPackage(),
-          new RNFirebaseLinksPackage(),
-          new RNFirebaseCrashlyticsPackage(),
-          new RNFirebasePerformancePackage(),
-          new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
-          new RNReactNativeHapticFeedbackPackage(),
-          new LinearGradientPackage(),
-          new FastImageViewPackage(),
-          new SplashScreenReactPackage(),
-          new RNCameraPackage(),
-          new RNDeviceInfo(),
-          new RNSharePackage(),
-          new ReactVideoPackage(),
-          new ReactNativeConfigPackage(),
-          new FBSDKPackage(mCallbackManager)
+          new MainReactPackage()
       );
     }
 
