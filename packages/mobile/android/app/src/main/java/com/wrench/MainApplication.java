11b811a33de683d9dd9c1e7aaf5b38b9aca2e9af
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-16 14:19:49 +0100

    Android react-native-share

--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -3,6 +3,7 @@ package com.wrench;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
           new RNGestureHandlerPackage(),
           new RNFirebasePackage(),
           new RNFirebaseAnalyticsPackage(),
