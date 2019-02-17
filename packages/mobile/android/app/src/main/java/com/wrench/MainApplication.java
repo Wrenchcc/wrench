bef945bdca5d8fb7ce8aab677bc3b4af20014002
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-17 18:48:43 +0100

    Android react-native-linear-gradient

--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -3,6 +3,7 @@ package com.wrench;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new RNReactNativeHapticFeedbackPackage(),
           new FBSDKPackage(mCallbackManager),
           new ReactNativeConfigPackage(),
