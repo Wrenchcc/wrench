0932de962e3f4c7893ca4fcd72774c3c7047e6f8
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-18 21:28:12 +0100

    Update dependencies

--- packages/mobile/src/features/project/components/Footer/styles.js
+++ packages/mobile/src/features/project/components/Footer/styles.js
@@ -3,10 +3,11 @@ import { COLORS } from 'ui/constants'
 import { isIphoneX } from 'utils/platform'
 
 export const Base = styled.View`
-  padding: 20px;
+  padding: 10px 20px 20px 20px;
+  padding-bottom: ${isIphoneX ? 40 : 20};
   height: ${isIphoneX ? 80 : 60};
   position: absolute;
-  bottom: 0;
+  bottom: ${isIphoneX ? -20 : 0};
   left: 0;
   right: 0;
   z-index: 10;
--- packages/mobile/src/features/project/containers/Project/index.js
+++ packages/mobile/src/features/project/containers/Project/index.js
@@ -12,7 +12,7 @@ import { TYPES } from 'ui/EmptyState/constants'
 import Header from 'features/project/components/Header'
 import Footer from 'features/project/components/Footer'
 
-const FOOTER_HEIGHT = 0
+const FOOTER_HEIGHT = 200
 
 let scrollView = null
 
