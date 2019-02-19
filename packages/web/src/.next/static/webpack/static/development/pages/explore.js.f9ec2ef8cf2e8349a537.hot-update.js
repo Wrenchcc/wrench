101024515fe1d1f7fd732e7276da48188b012c11
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-19 19:42:45 +0100

    Add next-js

---
+++ packages/web/src/.next/static/webpack/static/development/pages/explore.js.f9ec2ef8cf2e8349a537.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/explore.js",{
+
+/***/ "./pages/explore.ts":
+/*!**************************!*\
+  !*** ./pages/explore.ts ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Explore; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "../node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/pontus/Desktop/Wrench/Wrench/packages/web/src/pages/explore.ts";
+
+
+function Explore() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Explore");
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=explore.js.f9ec2ef8cf2e8349a537.hot-update.js.map
---
+++ packages/web/src/pages/explore.ts
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Explore() {
+  return (
+    <Link href="/explore">
+      <a>user</a>
+    </Link>
+  )
+}
