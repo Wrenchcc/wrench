225f224ff3f2b130f547b22b1e62996f82f56c9b
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-23 00:32:06 +0100

    Web CI

--- packages/web/server.ts
+++ packages/web/server.ts
@@ -3,7 +3,7 @@ import * as next from 'next'
 import { routes } from './routes'
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV === 'development'
 const app = next({ dev })
 const handle = routes.getRequestHandler(app)
 
