d016ef36a837fbd4155dabc182e8cc30e0249d84
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-22 23:33:01 +0100

    Build web

---
+++ packages/web/routes.ts
@@ -0,0 +1,9 @@
+import Routes, * as nextRoutes from 'next-routes'
+
+// @ts-ignore
+export const routes = nextRoutes() as Routes
+export const { Router } = routes
+export const { Link } = routes
+
+routes.add('user', '/user/:id')
+routes.add('explore', '/explore')
