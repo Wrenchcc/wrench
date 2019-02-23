e00df2c16eb20c5183090aa459c1b4d045be3c8a
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-23 11:39:07 +0100

    Handle environments

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,3 +1,20 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
 const withTypescript = require('@zeit/next-typescript')
 
-module.exports = withTypescript()
+module.exports = withTypescript({
+  webpack: config => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true,
+      }),
+    ]
+
+    return config
+  },
+})
