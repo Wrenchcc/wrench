52dcf95bb51913418bc92d0198fb90acbd249033
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-23 11:30:01 +0100

    Fix structure

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -5,5 +5,6 @@ export const routes = nextRoutes() as Routes
 export const { Router } = routes
 export const { Link } = routes
 
-routes.add('user', '/user/:id')
+routes.add('user', '/user/:slug')
+routes.add('project', '/project/:slug')
 routes.add('explore', '/explore')
--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -4,7 +4,7 @@ import { routes } from './routes'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV === 'development'
-const app = next({ dev })
+const app = next({ dev, dir: './src' })
 const handle = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
