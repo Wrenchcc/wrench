d5157c09fab77cadc7ef0c843066959fc94cdc6e
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-23 11:03:27 +0100

    Fix build

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -22,7 +22,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta charset="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
