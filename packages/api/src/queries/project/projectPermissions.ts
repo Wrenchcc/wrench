97b21b00ef023dd0ab2b28807e28a04e6f0aa409
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-25 09:08:50 +0100

    permission on project permission

--- packages/api/src/queries/project/projectPermissions.ts
+++ packages/api/src/queries/project/projectPermissions.ts
@@ -1,6 +1,4 @@
-import { isAuthenticated } from '../../utils/permissions'
-
-export default isAuthenticated(async ({ id, userId }, _, ctx) => ({
+export default async ({ id, userId }, _, ctx) => ({
   isFollower: await ctx.db.Following.isFollower(ctx.userId, id),
   isOwner: userId === ctx.userId,
-}))
+})
