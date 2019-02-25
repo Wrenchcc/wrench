fda908757caad4e54e26bae20ed79cc2f66303f4
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-25 15:28:33 +0100

    Add env to workflow

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,6 +3,7 @@ const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withTypescript = require('@zeit/next-typescript')
 
+console.log(process.env)
 module.exports = withPlugins([withTypescript, optimizedImages], {
   distDir: '../.next',
   env: {
