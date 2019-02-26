edf3ceb2dddbd4438754e8d1d75f4ac4ceaf0526
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-26 11:50:45 +0100

    Check for id on post

--- packages/api/src/queries/post/post.ts
+++ packages/api/src/queries/post/post.ts
@@ -1,2 +1,9 @@
 // TODO: Use dataloader
-export default async (_, { id }, ctx) => ctx.db.Post.findOne(id)
+export default async (_, { id }, ctx) => {
+  // Check for id when used in project
+  if (!id) {
+    return null
+  }
+
+  return ctx.db.Post.findOne(id)
+}
