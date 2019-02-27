daf76153bdb1413c9c4dedea4258595a5b1f12ff
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-27 21:47:14 +0100

    Handle auth

--- packages/web/src/pages/index.ts
+++ packages/web/src/pages/index.ts
@@ -5,28 +5,25 @@ import Seo from '../utils/seo'
 import { AUTHENTICATE_USER } from '../graphql/mutations/user/authenticate'
 
 export default function Home() {
+  const handleAuth = fb => useMutation(AUTHENTICATE_USER, {
+    update: (proxy, mutationResult) => {
+      console.log(mutationResult)
+    },
+    variables: {
+      facebookToken: fb.accessToken,
+      platform: 'WEB',
+    },
+  })
+
   return (
     <Fragment>
-      <Seo
-        config={{
-          title: 'Feed',
-        }}
-      />
+      <Seo config={{ title: 'Feed' }} />
       <FacebookLogin
         appId="1174076712654826"
         autoLoad
         fields="name,email,picture"
-        callback={({ accessToken }) => useMutation(AUTHENTICATE_USER, {
-          update: (proxy, mutationResult) => {
-            console.log(mutationResult)
-          },
-          variables: {
-            facebookToken: accessToken,
-            platform: 'WEB',
-          },
-        })
-        }
-        render={({ onClick }) => <button onClick={onClick}>Login with Facebook2</button>}
+        callback={handleAuth}
+        render={({ onClick }) => <button onClick={onClick}>Login with Facebook</button>}
       />
     </Fragment>
   )
