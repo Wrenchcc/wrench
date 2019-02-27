e2f8b901cb0673e5c6d1e7786dd7a0260dbab061
Pontus Abrahamsson <pontus@lostisland.co>
2019-02-27 21:26:24 +0100

    Add facebook sdk

--- packages/web/src/pages/index.ts
+++ packages/web/src/pages/index.ts
@@ -1,11 +1,30 @@
+import React, { Fragment } from 'react'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import Seo from '../utils/seo'
+
+const responseFacebook = response => {
+  console.log(response)
+}
+
+const onClick = response => {
+  console.log(response)
+}
 
 export default function Home() {
   return (
-    <Seo
-      config={{
-        title: 'Feed',
-      }}
-    />
+    <Fragment>
+      <Seo
+        config={{
+          title: 'Feed',
+        }}
+      />
+      <FacebookLogin
+        appId="1174076712654826"
+        autoLoad
+        fields="name,email,picture"
+        callback={responseFacebook}
+        render={({ onClick }) => <button onClick={onClick}>Login with Facebook2</button>}
+      />
+    </Fragment>
   )
 }
