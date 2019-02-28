f07bc754738d4323d021a11ce87fb967ab7a8888
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-02-28 12:23:22 +0100

    Check for tokens

--- packages/web/src/graphql/links/Auth/index.ts
+++ packages/web/src/graphql/links/Auth/index.ts
@@ -3,6 +3,6 @@ import { setContext } from 'apollo-link-context'
 export default tokens => setContext((_, { headers }) => ({
   headers: {
     ...headers,
-    authorization: `Bearer ${tokens.access_token}`,
+    ...(tokens && { authorization: `Bearer ${tokens.access_token}` }),
   },
 }))
