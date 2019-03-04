ffac6f6dee1184d7d86fb172eed50ee6e2fcfa48
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-03-04 10:11:55 +0100

    add cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import { ApolloServer } from 'apollo-server-express'
 import { createConnection } from 'typeorm'
 import { PostgresDriver } from 'typeorm/driver/postgres/PostgresDriver'
@@ -51,6 +52,8 @@ async function server() {
   })
 
   const app = express()
+
+  app.use(cors())
 
   server.applyMiddleware({ app })
 
