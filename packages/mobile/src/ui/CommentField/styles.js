21b8bc05c2c4d9486fe031ee2aa7b91e23c99e5d
Pontus Abrahamsson <pontus@lostisland.co>
2019-03-24 13:05:33 +0100

    Fix border in comment input

--- packages/mobile/src/ui/CommentField/styles.js
+++ packages/mobile/src/ui/CommentField/styles.js
@@ -1,13 +1,14 @@
+import { StyleSheet } from 'react-native'
+import styled from 'styled-components'
 import UiInput from 'ui/Input'
 import UiTouchable from 'ui/Touchable'
-import styled from 'styled-components'
 import { COLORS } from 'ui/constants'
 
 export const Base = styled.View`
   flex-direction: row;
   align-items: center;
   border-bottom-color: ${COLORS.DIVIDER};
-  border-bottom-width: 1;
+  border-bottom-width: ${StyleSheet.hairlineWidth};
 `
 
 export const Input = styled(UiInput)`
