89b5420b710005a7d18f12d476bd4b78fef3b34c
Pontus Abrahamsson <pontus@lostisland.co>
2019-03-24 20:45:51 +0100

    Fix Media item

--- packages/mobile/src/features/project/components/MediaPicker/Item/index.js
+++ packages/mobile/src/features/project/components/MediaPicker/Item/index.js
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
 import { Image, Dimensions } from 'react-native'
 import { Text, Touchable } from 'ui'
 import { COLORS } from 'ui/constants'
@@ -9,29 +10,34 @@ const { width } = Dimensions.get('window')
 const MARGIN = 3
 const IMAGE_SIZE = (width - 4 * MARGIN) / 4
 
-export default class Item extends PureComponent {
-  render() {
-    const { item, selected, onPress, order } = this.props
+const Item = memo(function Item({ item, selected, onPress, order }) {
+  return (
+    <Touchable
+      hapticFeedback="impactLight"
+      style={{ backgroundColor: COLORS.DARK_GREY, marginBottom: MARGIN, marginRight: MARGIN }}
+      onPress={() => onPress(item)}
+      activeOpacity={1}
+    >
+      <Image
+        source={{ uri: item.lowQualityUri || item.uri }}
+        style={{ height: IMAGE_SIZE, width: IMAGE_SIZE }}
+      />
 
-    return (
-      <Touchable
-        hapticFeedback="impactLight"
-        style={{ backgroundColor: COLORS.DARK_GREY, marginBottom: MARGIN, marginRight: MARGIN }}
-        onPress={() => onPress(item)}
-        activeOpacity={1}
-      >
-        <Image
-          source={{ uri: item.lowQualityUri || item.uri }}
-          style={{ height: IMAGE_SIZE, width: IMAGE_SIZE }}
-        />
+      <Marker selected={selected}>
+        <Text fontSize={12} bold>
+          {order || null}
+        </Text>
+      </Marker>
+      <Overlay selected={selected} />
+    </Touchable>
+  )
+})
 
-        <Marker selected={selected}>
-          <Text fontSize={12} bold>
-            {order || null}
-          </Text>
-        </Marker>
-        <Overlay selected={selected} />
-      </Touchable>
-    )
-  }
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+  order: PropTypes.number,
 }
+
+export default Item
