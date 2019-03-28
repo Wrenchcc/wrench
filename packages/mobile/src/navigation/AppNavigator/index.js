565239bd417fe12ec1f2565cf2c41c7700a2d6bb
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-03-28 21:44:48 +0100

    Handle notification when app is closed

--- packages/mobile/src/navigation/AppNavigator/index.js
+++ packages/mobile/src/navigation/AppNavigator/index.js
@@ -10,8 +10,9 @@ import Navigator from './navigator'
 
 class AppNavigator extends PureComponent {
   componentDidMount() {
-    this.createNotificationListeners()
+    this.createNotificationListener()
     this.createdynamicLinkListener()
+    this.createInitialNotificationListerer()
   }
 
   componentWillUnmount() {
@@ -26,7 +27,7 @@ class AppNavigator extends PureComponent {
     })
   }
 
-  createNotificationListeners() {
+  createNotificationListener() {
     // Get the action triggered by the notification being opened
     this.notificationOpenedListener = notifications().onNotificationOpened(({ notification }) => {
       if (notification.data) {
@@ -34,6 +35,15 @@ class AppNavigator extends PureComponent {
         Linking.canOpenURL(link).then(() => Linking.openURL(link))
       }
     })
+  }
+
+  async createInitialNotificationListerer() {
+    // If your app is closed
+    const notificationOpen = await notifications().getInitialNotification()
+    if (notificationOpen && notificationOpen.notification.data) {
+      const link = formatDeepLink(notificationOpen.notification.data.path)
+      Linking.canOpenURL(link).then(() => Linking.openURL(link))
+    }
   }
 
   render() {
