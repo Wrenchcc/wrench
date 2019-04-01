2bf782b9aba4a4f377a73ff6c0462d1a1faa83d6
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-04-01 10:10:43 +0200

    Use Sentry

--- packages/mobile/src/utils/storage/s3/index.js
+++ packages/mobile/src/utils/storage/s3/index.js
@@ -1,30 +1,43 @@
 import * as ImageManipulator from 'expo-image-manipulator'
 import { client } from 'graphql/createClient'
 import { PreSignUrlsMutation } from 'graphql/mutations/upload/preSignUrls'
+import { logError } from 'utils/sentry'
 import makeS3Request from './makeS3Request'
 
 async function cropImage({ uri, crop }) {
-  return ImageManipulator.manipulateAsync(uri, [{ crop }])
+  try {
+    return ImageManipulator.manipulateAsync(uri, [{ crop }])
+  } catch (err) {
+    logError(err)
+  }
+
+  return null
 }
 
 export const uploadFiles = async files => {
-  const input = files.map(() => ({ type: 'IMAGE' }))
-  // Return pre-signed urls
-  // Resize images and return uris
-  const [preSignedUrls, resizedImages] = await Promise.all([
-    client.mutate({ mutation: PreSignUrlsMutation, variables: { input } }),
-    Promise.all(files.map(cropImage)),
-  ])
+  try {
+    const input = files.map(() => ({ type: 'IMAGE' }))
+    // Return pre-signed urls
+    // Resize images and return uris
+    const [preSignedUrls, resizedImages] = await Promise.all([
+      client.mutate({ mutation: PreSignUrlsMutation, variables: { input } }),
+      Promise.all(files.map(cropImage)),
+    ])
 
-  // Return filenames
-  const result = await Promise.all(
-    resizedImages.map(async (uri, index) => {
-      const { url, type, filename } = preSignedUrls.data.preSignUrls[index]
-      const uploaded = await makeS3Request(url, { uri, type, filename })
+    // Return filenames
+    const result = await Promise.all(
+      resizedImages.map(async (uri, index) => {
+        const { url, type, filename } = preSignedUrls.data.preSignUrls[index]
+        const uploaded = await makeS3Request(url, { uri, type, filename })
 
-      return uploaded
-    })
-  )
+        return uploaded
+      })
+    )
 
-  return result
+    return result
+  } catch (err) {
+    logError(err)
+  }
+
+  return null
 }
