0e42c04df89671649dc312bc686b8704a15821bd
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-04-06 01:28:03 +0200

    Fix translations of notifications

--- packages/api/src/i18n/index.ts
+++ packages/api/src/i18n/index.ts
@@ -1,6 +1,5 @@
-import { DEFAULT_LOCALE } from '../utils/locale'
 import translations from './translations'
 
-export function translate({ key, params = {}, locale = DEFAULT_LOCALE }) {
-  return translations(params)[locale][key]
+export function translate({ key, params = {}, locale }) {
+  return translations(locale, params)[key]
 }
--- packages/api/src/i18n/translations.ts
+++ packages/api/src/i18n/translations.ts
@@ -1,16 +1,23 @@
-export default params => ({
-  en: {
-    NEW_COMMENT: `${params.name} commented: ${params.comment} on your project ${params.project}.`,
-    NEW_MENTION: `${params.name} mentioned you in a comment: ${params.comment}.`,
-    NEW_REPLY: `${params.name} replied to your comment: ${params.comment}.`,
-    NEW_FOLLOWER: `${params.name} started following your project: ${params.project}.`,
-  },
-  sv: {
-    NEW_COMMENT: `${params.name} kommenterade: ${params.comment} på ditt projekt ${
-      params.project
-    }.`,
-    NEW_MENTION: `${params.name} nämnde dig i en kommentar: ${params.comment} .`,
-    NEW_REPLY: `${params.name} svarade på din kommentar: ${params.comment} .`,
-    NEW_FOLLOWER: `${params.name} följer nu ditt projekt: ${params.project} .`,
-  },
-})
+export default (locale, params) => {
+  switch (locale) {
+    case 'sv':
+    case 'sv-SE':
+      return {
+        NEW_COMMENT: `${params.name} kommenterade: ${params.comment} på ditt projekt ${
+          params.project
+        }.`,
+        NEW_MENTION: `${params.name} nämnde dig i en kommentar: ${params.comment} .`,
+        NEW_REPLY: `${params.name} svarade på din kommentar: ${params.comment} .`,
+        NEW_FOLLOWER: `${params.name} följer nu ditt projekt: ${params.project} .`,
+      }
+    default:
+      return {
+        NEW_COMMENT: `${params.name} commented: ${params.comment} on your project ${
+          params.project
+        }.`,
+        NEW_MENTION: `${params.name} mentioned you in a comment: ${params.comment}.`,
+        NEW_REPLY: `${params.name} replied to your comment: ${params.comment}.`,
+        NEW_FOLLOWER: `${params.name} started following your project: ${params.project}.`,
+      }
+  }
+}
