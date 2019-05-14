437943c61dbb1ba5a42ec74e73b87854cc342527
Pontus Abrahamsson <pontus@lostisland.co>
2019-05-14 16:50:07 +0200

    Update dependencies

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -6,11 +6,11 @@ const withTypescript = require('@zeit/next-typescript')
 module.exports = withPlugins([withTypescript, optimizedImages], {
   distDir: '../.next',
   env: {
-    BUILD_ID: '123',
     // BUILD_ID: require('child_process') // eslint-disable-line
     //   .execSync('git rev-parse --short HEAD')
     //   .toString()
     //   .trim(),
+    BUILD_ID: '123',
     API_ENDPOINT:
       process.env.NODE_ENV === 'production'
         ? 'https://api.wrench.cc/graphql'
