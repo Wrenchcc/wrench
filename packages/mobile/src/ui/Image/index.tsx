f07ecde5f384d4c0c195fdccd77901d6cb6c30e2
Pontus Abrahamsson <pontus@lostisland.co>
2019-06-07 16:17:13 +0200

    WIP

--- packages/mobile/src/ui/Image/index.tsx
+++ packages/mobile/src/ui/Image/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { PixelRatio, Animated, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { IMAGE_PRIORITY } from 'ui/constants'
@@ -23,25 +23,11 @@ function Image({
     return null
   }
 
-  const [progress, setProgress] = useState(0)
-
-  const onProgress = evt => setProgress(evt.nativeEvent.loaded / evt.nativeEvent.total)
-
-  const imageAnimated = new Animated.Value(0)
-
-  const onImageLoad = () => {
-    Animated.timing(imageAnimated, {
-      duration: 200,
-      toValue: 1,
-      useNativeDriver: true,
-    }).start()
-  }
-
   const uri = `${source.uri}?w=${width}&h=${height}&dpr=${density}&webp=1`
 
   const placeholder = `${source.uri}?w=${Math.round(width / placeholderDensity)}&h=${Math.round(
     height / placeholderDensity
-  )}&dpr=1&blurRadius=10&webp=1`
+  )}&dpr=1`
 
   return (
     <Base
@@ -50,28 +36,27 @@ function Image({
       borderRadius={borderRadius}
       placeholderColor={placeholderColor}
     >
-      <AnimatedFastImage
+      <Animated.Image
         {...props}
-        source={{ uri: placeholder }}
+        source={{
+          uri: placeholder,
+        }}
         style={{ width, height }}
-        priority={priority || IMAGE_PRIORITY.NORMAL}
+        blurRadius={3}
       />
       <AnimatedFastImage
         {...props}
-        onProgress={onProgress}
         source={{ uri }}
         style={{
           bottom: 0,
           height,
           left: 0,
-          opacity: imageAnimated,
           position: 'absolute',
           right: 0,
           top: 0,
           width,
         }}
         priority={priority || IMAGE_PRIORITY.NORMAL}
-        onLoad={onImageLoad}
       />
     </Base>
   )
