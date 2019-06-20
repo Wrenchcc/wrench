40b58b89694f95d34b01811e02b7c0007e0e985d
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-06-20 15:10:57 +0200

    wip

--- packages/mobile/src/store/post.ts
+++ packages/mobile/src/store/post.ts
@@ -1,9 +1,7 @@
 import create from 'zustand'
+import { findIndex, propEq } from 'ramda'
 
 const MAX_SELECTED_FILES = 10
-// if (state.files.length === MAX_SELECTED_FILES) {
-//   return state
-// }
 
 const [usePostStore] = create(set => ({
   caption: null,
@@ -14,20 +12,38 @@ const [usePostStore] = create(set => ({
 
   actions: {
     onSelect: payload =>
-      set(state => ({
-        ...state,
-        files:
-          state.id === payload.id && state.files.some(file => file.id === payload.id)
-            ? state.files.filter(file => file.id !== payload.id)
-            : state.files.concat(payload),
-        id: payload.id, // TODO: Select next index when remove
-      })),
+      set(state => {
+        const currentId = payload.id
+        const isAdded = state.files.some(file => file.id === currentId)
+        const prevSelected = state.id === currentId
+        const index = findIndex(propEq('id', currentId))(state.files)
+        const id = currentId //prevSelected ? state.files[index - 1 || 0].id : payload.id
+
+        console.log(
+          state.files.length > 0
+            ? prevSelected
+              ? state.files[index - 1 || 0].id
+              : payload.id
+            : null
+        )
+
+        if (!prevSelected && !isAdded && state.files.length === MAX_SELECTED_FILES) {
+          return state
+        }
+
+        return {
+          files:
+            prevSelected && isAdded
+              ? state.files.filter(file => file.id !== currentId)
+              : state.files.concat(payload),
+          id,
+        }
+      }),
 
     onEdit: () => {},
 
     setIsPosting: payload => set({ isPosting: payload }),
     update: (field, payload) => set({ [field]: payload }),
-    reset: () => set(initialState),
   },
 }))
 
