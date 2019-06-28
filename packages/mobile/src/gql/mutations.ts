a9466dea31c3d0c7f4bedf307dae5ec11a2a5d90
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-06-28 15:35:43 +0200

    wip

--- packages/mobile/src/gql/mutations.ts
+++ packages/mobile/src/gql/mutations.ts
@@ -1,5 +1,35 @@
 import gql from 'graphql-tag'
 
+export const ADD_COMMENT_MUTATION = gql``
+
+export const DELETE_COMMENT_MUTATION = gql``
+
+export const MARK_ALL_NOTIFICATIONS_MUTATION = gql``
+
+export const MARK_NOTIFICATION_MUTATION = gql``
+
+export const ADD_POST_MUTATION = gql``
+
+export const DELETE_POST_MUTATION = gql``
+
+export const EDIT_POST_MUTATION = gql``
+
+export const ADD_PROJECT_MUTATION = gql``
+
+export const DELETE_PROJECT_MUTATION = gql``
+
+export const EDIT_PROJECT_MUTATION = gql``
+
+export const FOLLOW_PROJECT_MUTATION = gql``
+
+export const PRE_SING_URLS_MUTATION = gql``
+
+export const AUTHENTICATE_FACEBOOK_MUTATION = gql``
+
+export const EDIT_USER_MUTATION = gql``
+
 export const REFRESH_TOKEN_MUTATION = gql``
 
-export const ADD_COMMENT_MUTATION = gql``
+export const REGISTER_DEVICE_TOKEN_MUTATION = gql``
+
+export const TOGGLE_USER_NOTIFICATINS_SETTINGS_MUTATION = gql``
--- packages/mobile/src/gql/queries.ts
+++ packages/mobile/src/gql/queries.ts
@@ -2,4 +2,38 @@ import gql from 'graphql-tag'
 
 export const USER_QUERY = gql``
 
+export const USER_FOLLOWING_PROJECTS_QUERY = gql``
+
+export const CURRENT_USER_QUERY = gql``
+
+export const CURRENT_USER_PROJECTS_QUERY = gql``
+
+export const CURRENT_USER_SETTINGS_QUERY = gql``
+
+export const COMMENT_QUERY = gql``
+
+export const COMMENTS_QUERY = gql``
+
+export const COMMENTS_QUERY = gql``
+
+export const POST_QUERY = gql``
+
 export const PROJECT_QUERY = gql``
+
+export const PROJECT_SUGGESTIONS_QUERY = gql``
+
+export const PROJECT_TYPES_QUERY = gql``
+
+export const SEARCH_MODELS_QUERY = gql``
+
+export const SEARCH_PROJECTS_QUERY = gql``
+
+export const SEARCH_USERS_QUERY = gql``
+
+export const EXPLORE_QUERY = gql``
+
+export const FEED_QUERY = gql``
+
+export const FOLLOWERS_QUERY = gql``
+
+export const NOTIFICATIONS_QUERY = gql``
