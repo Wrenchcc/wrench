099880359b4befff99a3076af617f60606d69dd9
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-07-01 14:09:42 +0200

    links

--- packages/mobile/src/gql/client.ts
+++ packages/mobile/src/gql/client.ts
@@ -1,15 +1,10 @@
 import { ApolloClient } from 'apollo-client'
-import { ApolloLink } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import link from './links'
 import { clearTokens } from 'utils/storage/auth'
 import { track, events } from 'utils/analytics'
 import { LoginManager } from 'react-native-fbsdk'
 import { AuthNavigation } from 'navigation'
-import AuthLink from './links/Auth'
-import HttpLink from './links/Http'
-import OfflineLink from './links/Offline'
-import RefreshTokenLink from './links/RefreshToken'
-import RetryLink from './links/Retry'
 
 const cache = new InMemoryCache({
   freezeResults: true,
@@ -18,7 +13,7 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({
   assumeImmutableResults: true,
   cache,
-  link: ApolloLink.from([RetryLink, OfflineLink, AuthLink, RefreshTokenLink, HttpLink]),
+  link,
 })
 
 client.onResetStore(() => {
