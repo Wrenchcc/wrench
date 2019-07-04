33d97a10f10a94b19b8a88a2fae2085fff183018
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-07-04 12:53:40 +0200

    Use TouchableOpacity

--- packages/mobile/src/ui/Touchable/index.tsx
+++ packages/mobile/src/ui/Touchable/index.tsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import { BaseButton } from 'react-native-gesture-handler'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import evenHitSlop from 'utils/hitSlop'
 
-const Touchable = ({
-  activeOpacity = 0.8,
-  children,
-  hitSlop = 10,
-  nativeButton = true,
-  onPress,
-  ...props
-}) => {
-  const Button = nativeButton ? BaseButton : TouchableOpacity
-
+const Touchable = ({ activeOpacity = 0.8, children, hitSlop = 10, onPress, ...props }) => {
   return (
-    <Button
+    <TouchableOpacity
       activeOpacity={activeOpacity}
       onPress={onPress}
       hitSlop={evenHitSlop(hitSlop)}
       {...props}
     >
       {children}
-    </Button>
+    </TouchableOpacity>
   )
 }
 
