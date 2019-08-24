16e394160cd15899788df66c7303c5f53f6e359a
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-08-24 21:50:51 +0200

    Spark animation

--- packages/mobile/src/components/LikePost/index.tsx
+++ packages/mobile/src/components/LikePost/index.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
+import { Animated } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { useMutation, LIKE_POST_MUTATION } from 'gql'
 import { Icon, Text } from 'ui'
@@ -9,38 +10,53 @@ function LikePost({ post }) {
   const { t } = useTranslation()
   const [toggleLike] = useMutation(LIKE_POST_MUTATION)
 
-  const handleToggleLike = useCallback(
-    () =>
-      toggleLike({
-        variables: {
-          id: post.id,
-        },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          likePost: {
-            __typename: 'Post',
-            ...post,
-            likes: {
-              __typename: 'Likes',
-              isLiked: !post.likes.isLiked,
-              totalCount: post.likes.isLiked
-                ? post.likes.totalCount - 1
-                : post.likes.totalCount + 1,
-            },
+  const animatedValue = useRef(new Animated.Value(0))
+
+  const scale = animatedValue.current.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 1.2, 1],
+  })
+
+  const handleToggleLike = useCallback(() => {
+    animatedValue.current.setValue(0)
+
+    Animated.spring(animatedValue.current, {
+      toValue: 1,
+      duration: 330,
+      useNativeDriver: true,
+    }).start()
+
+    toggleLike({
+      variables: {
+        id: post.id,
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likePost: {
+          __typename: 'Post',
+          ...post,
+          likes: {
+            __typename: 'Likes',
+            isLiked: !post.likes.isLiked,
+            totalCount: post.likes.isLiked ? post.likes.totalCount - 1 : post.likes.totalCount + 1,
           },
         },
-      }),
-    [toggleLike]
-  )
+      },
+    })
+  }, [toggleLike])
+
   return (
     <Base>
-      <Icon
-        source={spark}
-        color={post.likes.isLiked ? 'spark' : 'dark'}
-        style={{ marginRight: 10 }}
-        onPress={handleToggleLike}
-        hapticFeedback="impactLight"
-      />
+      <Animated.View style={{ transform: [{ scale }] }}>
+        <Icon
+          source={spark}
+          color={post.likes.isLiked ? 'spark' : 'dark'}
+          style={{ marginRight: 10 }}
+          onPress={handleToggleLike}
+          hapticFeedback="impactLight"
+        />
+      </Animated.View>
+
       <Text fontSize={15}>{t('LikePost:like', { count: post.likes.totalCount })}</Text>
     </Base>
   )
