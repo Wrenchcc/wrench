095dd3c1918309423eaba0882c4e9e7912c2dcf9
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-09-10 11:58:16 +0200

    Update device info

--- packages/mobile/src/i18n/helpers.ts
+++ packages/mobile/src/i18n/helpers.ts
@@ -1,14 +1,17 @@
 import AsyncStorage from '@react-native-community/async-storage'
-import DeviceInfo from 'react-native-device-info'
+import { NativeModules } from 'react-native'
+import { isIphone } from 'utils/platform'
 import { SELECTED_LOCALE_KEY } from 'utils/storage/constants'
 
 export const setLocale = locale => AsyncStorage.setItem(SELECTED_LOCALE_KEY, locale)
 
 export const getLocale = async () => {
-  const deviceLocale = DeviceInfo.getDeviceLocale()
+  const deviceLocale = isIphone
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier
   const savedLocale = await AsyncStorage.getItem(SELECTED_LOCALE_KEY)
 
   return savedLocale || deviceLocale
 }
 
-export const timezone = DeviceInfo.getTimezone()
+export const timezone = null // DeviceInfo.getTimezone()
