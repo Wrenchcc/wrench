c7d33042c9906a853327b20a9b872eeb9a1bcef9
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-09-11 08:48:01 +0200

    Remove firebase dynamic links

--- packages/mobile/src/utils/dynamicLinks/index.ts
+++ packages/mobile/src/utils/dynamicLinks/index.ts
@@ -1,16 +1,15 @@
-import qs from 'url'
+// import qs from 'url'
 import { navigateTo, SCREENS, SCREEN_PATH } from 'navigation'
 
 export const handleDynamicLink = link => {
-  const { path } = qs.parse(link)
-  const [route, value] = path.substring(1).split('/')
-
-  switch (route) {
-    case SCREEN_PATH.PROJECT:
-      return navigateTo(SCREENS.PROJECT, { slug: value })
-    default:
-      return navigateTo(SCREENS.USER, { user: { username: route } })
-  }
+  // const { path } = qs.parse(link)
+  // const [route, value] = path.substring(1).split('/')
+  // switch (route) {
+  //   case SCREEN_PATH.PROJECT:
+  //     return navigateTo(SCREENS.PROJECT, { slug: value })
+  //   default:
+  //     return navigateTo(SCREENS.USER, { user: { username: route } })
+  // }
 }
 
 export const handlePushNotification = data => {
