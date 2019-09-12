4e96a1a10f93e9b5ed3ab86cb3985a75904ca67f
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-09-12 11:54:15 +0200

    Optimize images

--- packages/mobile/src/utils/dynamicLinks/index.ts
+++ packages/mobile/src/utils/dynamicLinks/index.ts
@@ -1,38 +1,35 @@
 import { Linking } from 'react-native'
 import { navigateTo, SCREENS } from 'navigation'
-import Routes from './Routes'
+import DeepLinking from 'react-native-deep-linking'
 
 export const createDeepLinkingHandler = ({ url }) => {
   Linking.canOpenURL(url).then(supported => {
     if (supported) {
-      Routes.evaluateUrl(url)
+      DeepLinking.evaluateUrl(url)
     }
   })
 }
 
-Routes.addScheme('wrench://')
-Routes.addScheme('https://wrench.cc')
+DeepLinking.addScheme('wrench://')
+DeepLinking.addScheme('https://wrench.cc')
 
-Routes.addRoute('/project/:slug', ({ slug }) => {
+DeepLinking.addRoute('/project/:slug', ({ slug }) => {
   return navigateTo(SCREENS.PROJECT, { slug })
 })
 
-Routes.addRoute('/:root', ({ root }) => {
-  if (root === SCREENS.FEED) {
-    return navigateTo(SCREENS.FEED)
+DeepLinking.addRoute('/:root', ({ root }) => {
+  switch (root) {
+    case SCREENS.FEED:
+      return navigateTo(SCREENS.FEED)
+    case SCREENS.EXPLORE:
+      return navigateTo(SCREENS.EXPLORE)
+    case SCREENS.NOTIFICATIONS:
+      return navigateTo(SCREENS.NOTIFICATIONS)
+    default:
+      return navigateTo(SCREENS.USER, {
+        user: {
+          username: root,
+        },
+      })
   }
-
-  if (root === SCREENS.EXPLORE) {
-    return navigateTo(SCREENS.EXPLORE)
-  }
-
-  if (root === SCREENS.NOTIFICATIONS) {
-    return navigateTo(SCREENS.NOTIFICATIONS)
-  }
-
-  return navigateTo(SCREENS.USER, {
-    user: {
-      username: root,
-    },
-  })
 })
