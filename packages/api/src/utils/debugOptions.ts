e28a81131f59347b496c63b05ee542eb8e9eca6f
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-10-22 20:24:49 +0200

    Disable debug

--- packages/api/src/utils/debugOptions.ts
+++ packages/api/src/utils/debugOptions.ts
@@ -1,10 +1,10 @@
 const { NODE_ENV } = process.env
 
-export default (NODE_ENV !== 'production'
+export default NODE_ENV !== 'production'
   ? {
       debug: true,
       introspection: true,
       playground: true,
       tracing: true,
     }
-  : {})
+  : {}
