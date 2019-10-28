ef71faf2fb6a3d8749e08b376470ebbf38256ac7
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-10-28 19:15:28 +0100

    Add debug to elastisearch

--- packages/api/src/services/elasticsearch/client.ts
+++ packages/api/src/services/elasticsearch/client.ts
@@ -1,28 +1,34 @@
 import * as aws4 from 'aws4'
 import axios from 'axios'
 
+const debug = require('debug')('api:service:elasticsearch')
+
 const { ELASTICSEARCH_DOMAIN, NODE_ENV } = process.env
 
 export default async ({ body = null, path }) => {
-  const options = {
-    body,
-    data: body,
-    url: `https://${ELASTICSEARCH_DOMAIN}/${path}`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    host: ELASTICSEARCH_DOMAIN,
-    method: 'POST',
-    path,
-  }
-
-  if (NODE_ENV !== 'production') {
-    return axios({
-      ...options,
+  try {
+    const options = {
+      body,
+      data: body,
+      url: `https://${ELASTICSEARCH_DOMAIN}/${path}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      host: ELASTICSEARCH_DOMAIN,
       method: 'POST',
-      url: `http://${ELASTICSEARCH_DOMAIN}/${path}`,
-    })
-  }
+      path,
+    }
 
-  return axios(aws4.sign(options))
+    if (NODE_ENV !== 'production') {
+      return axios({
+        ...options,
+        method: 'POST',
+        url: `http://${ELASTICSEARCH_DOMAIN}/${path}`,
+      })
+    }
+
+    return axios(aws4.sign(options))
+  } catch (err) {
+    debug(err)
+  }
 }
