ee91aee8c27ab9d604c8dcf956af151e80aae4e9
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-03 22:29:23 +0100

    Update dependencies

--- packages/api/src/mutations/upload/preSignUrl.ts
+++ packages/api/src/mutations/upload/preSignUrl.ts
@@ -5,11 +5,15 @@ import { isAuthenticated } from '../../utils/permissions'
 
 const debug = require('debug')('api:preSignUrl')
 
+const { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } = process.env
+
 const AWS_S3_BUCKET = 'wrench-files'
 const AWS_S3_REGION = 'us-east-1'
 
 const s3 = new S3({
+  accessKeyId: AWS_ACCESS_KEY,
   region: AWS_S3_REGION,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
   signatureVersion: 'v4',
   useAccelerateEndpoint: true,
 })
