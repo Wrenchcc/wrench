695e761778c6a47cf7dc58af66df12ae79b16329
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-03 21:27:51 +0100

    debug pre sign urls

--- packages/api/src/mutations/upload/preSignUrls.ts
+++ packages/api/src/mutations/upload/preSignUrls.ts
@@ -1,13 +1,24 @@
+import { S3 } from 'aws-sdk'
 import { v4 } from 'uuid'
 import getExtFromType from '../../utils/getExtFromType'
 import { isAuthenticated } from '../../utils/permissions'
 
 const debug = require('debug')('api:preSignUrls')
 
+const { AWS_S3_REGION, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } = process.env
+
 const AWS_S3_BUCKET = 'wrench-files'
 const UPLOAD_DIRECTORY = 'images'
 
-export default isAuthenticated(async (_, { input }, ctx) => {
+const s3 = new S3({
+  accessKeyId: AWS_ACCESS_KEY,
+  region: AWS_S3_REGION,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4',
+  useAccelerateEndpoint: true,
+})
+
+export default isAuthenticated(async (_, { input }) => {
   try {
     return Promise.all(
       input.map(async file => {
@@ -15,7 +26,7 @@ export default isAuthenticated(async (_, { input }, ctx) => {
         const filename = `${v4()}.${type}`
 
         try {
-          const url = await ctx.services.s3.getSignedUrl({
+          const url = await s3.getSignedUrl('putObject', {
             Bucket: AWS_S3_BUCKET,
             Key: `${UPLOAD_DIRECTORY}/${filename}`,
           })
