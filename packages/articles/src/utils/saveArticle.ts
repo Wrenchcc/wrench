ccf37f093f80d918f33fcc7e3f3878d88b5db078
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-05 11:09:24 +0100

    Remove articles

--- packages/articles/src/utils/saveArticle.ts
+++ packages/articles/src/utils/saveArticle.ts
@@ -1,6 +1,4 @@
 import * as Parser from 'rss-parser'
-import { connection, db } from '../models'
-import uploadToS3 from './uploadToS3'
 import extractImageSources from './extractImageSources'
 import stripNewLines from './stripNewLines'
 
@@ -11,57 +9,34 @@ const parser = new Parser({
 })
 
 export default async function saveArticle(feed, provider) {
-  await connection()
-
   try {
     let images
     const response = await parser.parseURL(feed)
 
     const item = response.items[0]
 
-    const article = await db.Article.findOne({ where: { url: item.link } })
-
-    if (!article) {
-      switch (provider) {
-        case 'returnofthecaferacers': {
-          images = [item['media:content'].$.url]
-        }
-        case 'themotoblogs': {
-          images = [item.content.$.url]
-        }
-        default: {
+    switch (provider) {
+      case 'returnofthecaferacers': {
+        images = [item['media:content'].$.url]
+      }
+      case 'themotoblogs': {
+        images = [item.content.$.url]
+      }
+      default:
+        {
           images = extractImageSources(item['content:encoded'])
         }
-      }
 
-      const uploadedFiles = await uploadToS3(images)
-      if (uploadedFiles.length > 0) {
-        const [files, author, categories, publisher] = await Promise.all([
-          db.ArticleFile.save(uploadedFiles),
-          db.ArticleAuthor.findOrCreate(item.creator),
-          db.ArticleCategory.findOrCreate(item.categories),
-          db.ArticlePublisher.findOne({ where: { slug: provider } }),
-        ])
-
-        await db.ArticlePublisher.update(publisher.id, { updatedAt: new Date(item.isoDate) })
-
-        const data = await db.Article.save({
-          author,
+        return {
+          provider,
+          images,
+          categories: item.categories,
+          author: item.creator,
           createdAt: new Date(item.isoDate),
           description: stripNewLines(item.contentSnippet),
-          files,
-          publisher,
           title: item.title,
           url: item.link,
-        })
-
-        const articleCategories = categories.map(({ id }) => ({
-          articleId: data.id,
-          categoryId: id,
-        }))
-
-        await db.ArticleCategoryRelationships.save(articleCategories)
-      }
+        }
     }
   } catch (err) {
     console.log(err)
--- packages/articles/src/utils/saveArticle2.ts
+++ packages/articles/src/utils/saveArticle2.ts
@@ -1,42 +1,43 @@
-import { connection, db } from '../models'
-import uploadToS3 from './uploadToS3'
 import stripNewLines from './stripNewLines'
 
 export default async function saveArticle(feed, provider, item, images) {
-  await connection()
-
   try {
-    const article = await db.Article.findOne({ where: { url: item.link } })
+    // if (uploadedFiles.length > 0) {
+    //   const [files, author, categories, publisher] = await Promise.all([
+    //     db.ArticleFile.save(uploadedFiles),
+    //     db.ArticleAuthor.findOrCreate(item.creator),
+    //     db.ArticleCategory.findOrCreate(item.categories),
+    //     db.ArticlePublisher.findOne({ where: { slug: provider } }),
+    //   ])
 
-    if (!article) {
-      const uploadedFiles = await uploadToS3(images)
+      // await db.ArticlePublisher.update(publisher.id, { updatedAt: new Date(item.isoDate) })
 
-      if (uploadedFiles.length > 0) {
-        const [files, author, categories, publisher] = await Promise.all([
-          db.ArticleFile.save(uploadedFiles),
-          db.ArticleAuthor.findOrCreate(item.creator),
-          db.ArticleCategory.findOrCreate(item.categories),
-          db.ArticlePublisher.findOne({ where: { slug: provider } }),
-        ])
+      // const data = await db.Article.save({
+      //   author,
+      //   createdAt: new Date(item.isoDate),
+      //   description: stripNewLines(item.contentSnippet),
+      //   files,
+      //   publisher,
+      //   title: item.title,
+      //   url: item.link,
+      // })
 
-        await db.ArticlePublisher.update(publisher.id, { updatedAt: new Date(item.isoDate) })
+      // const articleCategories = categories.map(({ id }) => ({
+      //   articleId: data.id,
+      //   categoryId: id,
+      // }))
 
-        const data = await db.Article.save({
-          author,
-          createdAt: new Date(item.isoDate),
-          description: stripNewLines(item.contentSnippet),
-          files,
-          publisher,
-          title: item.title,
-          url: item.link,
-        })
+      // await db.ArticleCategoryRelationships.save(articleCategories)
 
-        const articleCategories = categories.map(({ id }) => ({
-          articleId: data.id,
-          categoryId: id,
-        }))
-
-        await db.ArticleCategoryRelationships.save(articleCategories)
+      return {
+        provider,
+        images,
+        categories: articleCategories,
+        author: item.creator,
+        createdAt: new Date(item.isoDate),
+        description: stripNewLines(item.contentSnippet),
+        title: item.title,
+        url: item.link,
       }
     }
   } catch (err) {
