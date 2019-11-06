df5cfd8209bdad6aa9b1aaa0fc97a83f2e37a645
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-06 08:49:37 +0100

    Disable redis

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import responseCachePlugin from 'apollo-server-plugin-response-cache'
+// import responseCachePlugin from 'apollo-server-plugin-response-cache'
 import { createConnection } from 'typeorm'
 import { PostgresDriver } from 'typeorm/driver/postgres/PostgresDriver'
 import depthLimit from 'graphql-depth-limit'
@@ -12,7 +12,7 @@ import { options, db } from './models'
 import createLoaders from './loaders'
 import services from './services'
 import onHealthCheck from './utils/onHealthCheck'
-import { client as RedisClient } from './services/redis'
+// import { client as RedisClient } from './services/redis'
 
 const debug = require('debug')('api:server')
 
@@ -34,15 +34,15 @@ async function server() {
       // Cache everything for at least a minute since we only cache public responses
       defaultMaxAge: 60,
     },
-    cache: RedisClient(),
-    plugins: [
-      responseCachePlugin({
-        sessionId: ({ context }) => (context.userId ? context.userId : null),
-        // Only cache public responses
-        shouldReadFromCache: ({ context }) => !context.userId,
-        shouldWriteToCache: ({ context }) => !context.userId,
-      }),
-    ],
+    // cache: RedisClient(),
+    // plugins: [
+    //   responseCachePlugin({
+    //     sessionId: ({ context }) => (context.userId ? context.userId : null),
+    //     // Only cache public responses
+    //     shouldReadFromCache: ({ context }) => !context.userId,
+    //     shouldWriteToCache: ({ context }) => !context.userId,
+    //   }),
+    // ],
     context: ({ req }) => ({
       db,
       loaders: createLoaders(),
