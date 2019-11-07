b9d5da0213f2d6a5a6e017d7cff45578e877286e
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-07 14:45:08 +0100

    Remove sns from context

--- packages/api/src/mutations/invite/sendPromo.ts
+++ packages/api/src/mutations/invite/sendPromo.ts
@@ -1,15 +1,16 @@
 import { UserInputError } from 'apollo-server-express'
+import { sendSMS } from '../../services/sns'
 
 const message =
   'Click here to download Wrench for iPhone: http://appstore.com/cc.wrench.app Android: https://play.google.com/store/apps/details?id=com.wrench'
 
-export default async (_, { number }, ctx) => {
+export default async (_, { number }) => {
   if (!number) {
     return new UserInputError('Not a supported number.')
   }
 
   try {
-    await ctx.services.sns.sendSMS(number, message)
+    await sendSMS(number, message)
   } catch (err) {
     return new UserInputError('Something went wrong.')
   }
