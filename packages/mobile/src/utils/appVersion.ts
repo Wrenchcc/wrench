c8c77e407c82ecb10105aae0a9be5ae9513e4f0d
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-08 14:49:24 +0100

    App version

--- packages/mobile/src/utils/appVersion.ts
+++ packages/mobile/src/utils/appVersion.ts
@@ -1,3 +1,4 @@
 import DeviceLocale from 'react-native-device-locale'
+import Config from 'react-native-config'
 
-export default `v${DeviceLocale.buildNumber}`
+export default `v${DeviceLocale.buildNumber} (${Config.GITHUB_HASH})`
