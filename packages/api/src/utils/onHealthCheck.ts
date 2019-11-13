3f7b2591bcc6f5cc231b76ae058ec6d041514735
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-13 22:42:45 +0100

    disable ElasticSearchHealthCheck

--- packages/api/src/utils/onHealthCheck.ts
+++ packages/api/src/utils/onHealthCheck.ts
@@ -1,7 +1,7 @@
 import { default as DatabaseHealthCheck } from '../models/healthCheck'
-import { healthCheck as ElasticSearchHealthCheck } from '../services/elasticsearch'
+// import { healthCheck as ElasticSearchHealthCheck } from '../services/elasticsearch'
 
 export default async function onHealthCheck() {
-  await ElasticSearchHealthCheck()
+  // await ElasticSearchHealthCheck()
   await DatabaseHealthCheck()
 }
