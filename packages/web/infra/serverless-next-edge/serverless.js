e70c4d3fb4a118acb5425e3b84940c2ed839c721
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-17 22:43:49 +0100

    API

--- packages/web/infra/serverless-next-edge/serverless.js
+++ packages/web/infra/serverless-next-edge/serverless.js
@@ -298,7 +298,7 @@ class NextjsComponent extends Component {
       apiEdgeLambdaPublishOutputs = await apiEdgeLambda.publishVersion()
 
       cloudFrontOrigins[0].pathPatterns['api/*'] = {
-        // ttl: 5,
+        ttl: 0,
         'lambda@edge': {
           'origin-request': `${apiEdgeLambdaOutputs.arn}:${apiEdgeLambdaPublishOutputs.version}`,
         },
@@ -325,7 +325,7 @@ class NextjsComponent extends Component {
 
     const cloudFrontOutputs = await cloudFront({
       defaults: {
-        // ttl: 5,
+        ttl: 0,
         allowedHttpMethods: ['HEAD', 'GET', 'POST'],
         ...(inputs.cloudfront || {}),
         forward: {
