46a802ebc4388312c24e6417c086ee4647a59d4b
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-17 13:57:49 +0100

    Apple sign in redirect uri

--- packages/web/src/components/Login/index.tsx
+++ packages/web/src/components/Login/index.tsx
@@ -10,7 +10,7 @@ import { AUTHENTICATE_FACEBOOK, AUTHENTICATE_GOOGLE } from 'graphql/mutations/us
 import AppleSignIn from '../AppleSignIn'
 import { Base, FacebookButton, AppleButton, GoogleButton } from './styles'
 
-const { APPLE_REDRECT_URI } = process.env
+const { APPLE_REDIRECT_URI } = process.env
 
 const FACEBOOK_APP_ID = '1174076712654826'
 const FACEBOOK_SCOPE = 'public_profile,email'
@@ -49,7 +49,7 @@ export default function Login({ closeModal }) {
         )}
       />
 
-      <AppleSignIn clientId="cc.wrench" scope="name email" redirectURI={APPLE_REDRECT_URI}>
+      <AppleSignIn clientId="cc.wrench" scope="name email" redirectURI={APPLE_REDIRECT_URI}>
         {({ signIn }) => <AppleButton onClick={signIn}>{t('Login:applebutton')}</AppleButton>}
       </AppleSignIn>
 
