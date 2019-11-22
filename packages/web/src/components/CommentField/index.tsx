e09e8b32e0ff0df1b160c359e439efdeeb485dae
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-22 12:05:38 +0100

    Mentions

--- packages/web/src/components/CommentField/index.tsx
+++ packages/web/src/components/CommentField/index.tsx
@@ -1,12 +1,14 @@
 // @ts-nocheck
 import React, { memo, useState, useEffect, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import { Mention as M, MentionsInput } from 'react-mentions'
+import { useQuery, useLazyQuery, useMutation } from '@apollo/react-hooks'
 import { prepend, append } from 'ramda'
 import optimisticId from 'utils/optimisticId'
 import { CURRENT_USER } from 'graphql/queries/user/currentUser'
 import { GET_COMMENTS } from 'graphql/queries/comments'
 import { ADD_COMMENT_MUTATION } from 'graphql/mutations/comment/addComment'
+import { SEARCH_USER } from 'graphql/queries/search/searchUser'
 import postInfo from 'graphql/fragments/post/postInfo'
 import commentInfo from 'graphql/fragments/comment/commentInfo'
 import { Avatar, Text } from 'ui'
@@ -14,10 +16,59 @@ import { COLORS } from 'ui/constants'
 import { useCookie, Cookies } from 'hooks'
 import { useModal, Modal } from 'ui/Modal'
 import Login from 'components/Login'
-import { Base, Inner, Input, Button } from './styles'
+import { Base, Inner, Button, Username } from './styles'
 import { useRouter } from 'next/router'
 
 const DEFAULT_AVATAR_URL = 'https://edge-files.wrench.cc/avatar/default.jpg'
+
+const styles = {
+  width: '100%',
+  display: 'flex',
+  padding: 0,
+
+  suggestions: {
+    top: 20,
+    left: 0,
+    width: 375,
+    boxShadow: '0 4px 10px 0 rgba(0, 0, 0, 0.11)',
+    backgroundColor: 'white',
+
+    list: {
+      backgroundColor: 'white',
+      border: 'none',
+      fontSize: 16,
+      color: 'black',
+      position: 'absolute',
+      width: 375,
+      maxHeight: 368,
+      overflow: 'auto',
+    },
+
+    item: {
+      padding: '0 20px',
+      boxSizing: 'border-box',
+      height: 70,
+      borderBottom: '1px solid rgba(0,0,0,0.15)',
+
+      '&focused': {
+        backgroundColor: '#e6e7e9',
+      },
+    },
+  },
+
+  input: {
+    border: 0,
+    width: '100%',
+    outline: 'none',
+    fontSize: 16,
+    color: COLORS.DARK,
+    marginLeft: 10,
+    display: 'flex',
+    padding: 0,
+    top: 5,
+    height: 30,
+  },
+}
 
 const CommentField = React.forwardRef(({ postId, commentId, initialValue = '' }, ref) => {
   const { t } = useTranslation()
@@ -29,7 +80,41 @@ const CommentField = React.forwardRef(({ postId, commentId, initialValue = '' },
     skip: !isAuthenticated,
   })
 
+  const [searchUser, { data }] = useLazyQuery(SEARCH_USER)
   const [addCommentMutation] = useMutation(ADD_COMMENT_MUTATION)
+
+  async function fetchUsers(query, callback) {
+    if (!query) return
+
+    searchUser({
+      variables: {
+        query,
+        type: 'USERS',
+      },
+    })
+
+    if (data) {
+      callback(
+        data.users.edges.map(({ node }) => ({
+          id: node.id,
+          display: node.username,
+          avatarUrl: node.avatarUrl,
+        }))
+      )
+    }
+  }
+
+  const renderItem = ({ id, display, avatarUrl }) => {
+    return (
+      <div
+        key={id}
+        style={{ display: 'flex', height: 70, flexDirection: 'row', alignItems: 'center' }}
+      >
+        <Avatar size={40} uri={avatarUrl} />
+        <Username medium>{display}</Username>
+      </div>
+    )
+  }
 
   useEffect(() => {
     setText(initialValue)
@@ -230,15 +315,24 @@ const CommentField = React.forwardRef(({ postId, commentId, initialValue = '' },
       <Inner>
         <Avatar uri={(currentUser.data && currentUser.data.user.avatarUrl) || DEFAULT_AVATAR_URL} />
 
-        <Input
+        <MentionsInput
+          style={styles}
           ref={ref}
           placeholder={t('CommentField:placeholder')}
-          placeholderTextColor={COLORS.LIGHT_GREY}
           onChange={handleOnChangeText}
           onFocus={handleFocus}
           value={text}
           onKeyDown={handleOnKeyDown}
-        />
+        >
+          <M
+            displayTransform={(id, display) => `@${display}`}
+            trigger="@"
+            data={fetchUsers}
+            style={{ left: 0 }}
+            renderSuggestion={renderItem}
+          />
+        </MentionsInput>
+
         {text.length > 0 && (
           <Button onClick={handleSubmit}>
             <Text fontSize={15} medium>
--- packages/web/src/components/CommentField/styles.ts
+++ packages/web/src/components/CommentField/styles.ts
@@ -1,9 +1,13 @@
 // @ts-nocheck
 import styled from 'styled-components'
-import { COLORS } from 'ui/constants'
+import UiText from 'ui/Text'
 
 export const Base = styled.div`
   margin-top: 20px;
+`
+
+export const Username = styled(UiText)`
+  margin-left: 10px;
 `
 
 export const Button = styled.button`
@@ -16,20 +20,4 @@ export const Inner = styled.div`
   align-items: center;
   display: flex;
   width: 100%;
-`
-
-export const Input = styled.input`
-  border: 0;
-  width: 100%;
-  outline: none;
-  font-size: 16px;
-  color: ${COLORS.DARK};
-  margin-bottom: 10px;
-  margin-left: 10px;
-  display: flex;
-  padding-top: 6px;
-
-  &::-webkit-input-placeholder {
-    color: ${COLORS.LIGHT_GREY};
-  }
 `
