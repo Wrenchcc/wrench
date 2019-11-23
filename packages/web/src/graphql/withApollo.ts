94ee8171e3ec92b2e2f0e3a83e858ff22f6a3da1
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-23 16:38:00 +0100

    Cookies

--- packages/web/src/graphql/withApollo.ts
+++ packages/web/src/graphql/withApollo.ts
@@ -3,10 +3,7 @@ import Apollo from 'services/apollo'
 import Cookie from 'services/cookie'
 
 export default withApollo(({ headers, initialState }) => {
-  if (headers && headers.cookie) {
-    Cookie.init(headers && headers.cookie)
-  }
-
+  Cookie.init(headers && headers.cookie)
   Apollo.init(initialState)
   return Apollo.getClient()
 })
