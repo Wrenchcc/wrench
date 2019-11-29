944ebb73eae7eedda36c63f34f7def6ada5a1f8b
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-11-29 23:25:55 +0100

    Edit user

--- packages/api/src/mutations/user/editUser.ts
+++ packages/api/src/mutations/user/editUser.ts
@@ -3,7 +3,10 @@ import { DateTime } from 'luxon'
 import { isAuthenticated } from '../../utils/permissions'
 import { LOCALE_COLUMN, TIMEZONE_COLUMN } from '../../models/UserSettings'
 
+// NOTE: If input is empty string the value will be deleted
 export default isAuthenticated(async (_, args, ctx) => {
+  let data = {}
+
   if (args.input.interestedIn) {
     const interestedIn = args.input.interestedIn.map(({ id }) => ({
       projectTypeId: id,
@@ -59,31 +62,75 @@ export default isAuthenticated(async (_, args, ctx) => {
     }
   }
 
-  await ctx.db.User.update(ctx.userId, {
-    location: args.input.location || null,
-    bio: args.input.bio || null,
-    website: args.input.website || null,
-    avatarUrl: args.input.avatarUrl || null,
-    isSilhouette: !args.input.avatarUrl,
-  })
-
   if (args.input.firstName) {
-    await ctx.db.User.update(ctx.userId, {
+    data = {
+      ...data,
       firstName: args.input.firstName,
-    })
+    }
   }
 
   if (args.input.lastName) {
-    await ctx.db.User.update(ctx.userId, {
+    data = {
+      ...data,
       lastName: args.input.lastName,
-    })
+    }
   }
 
-  if (args.input.firstName || args.input.lastName) {
-    await ctx.db.User.update(ctx.userId, {
-      fullName: `${args.input.firstName} ${args.input.lastName}`,
-    })
+  if (args.input.bio) {
+    data = {
+      ...data,
+      bio: args.input.bio,
+    }
   }
+
+  if (args.input.location) {
+    data = {
+      ...data,
+      location: args.input.location,
+    }
+  }
+
+  if (args.input.website) {
+    data = {
+      ...data,
+      website: args.input.website,
+    }
+  }
+
+  if (args.input.avatarUrl) {
+    data = {
+      ...data,
+      isSilhouette: false,
+      avatarUrl: args.input.avatarUrl,
+    }
+  }
+
+  if (args.input.firstName && args.input.lastName) {
+    data = {
+      ...data,
+      fullName: `${args.input.firstName} ${args.input.lastName}`,
+    }
+  }
+
+  // Get empty input fields
+  const keys = Object.keys(args.input)
+  const emptyFields = keys.filter(key => args.input[key] === '')
+
+  if (emptyFields.length > 0) {
+    const nullObject = emptyFields.reduce((data, val) => {
+      if (val) {
+        // @ts-ignore
+        data.isSilhouette = true
+      }
+
+      data[val] = null
+      return data
+    }, {})
+
+    await ctx.db.User.update(ctx.userId, nullObject)
+  }
+
+  await ctx.db.User.update(ctx.userId, data)
 
   return ctx.loaders.user.load(ctx.userId)
 })
