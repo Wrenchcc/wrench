c301e913aeab886a6ef67413217ad7f1d2b13cb3
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-12-15 14:41:14 +0100

    Bundle splitting

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -6,25 +6,25 @@ const fs = require('fs')
 const ROOT_FOLDER = resolve(__dirname, '../../')
 
 const config = {
-  transformer: {
-    // getTransformOptions: () => {
-    //   const moduleMap = {}
-    //   modulePaths.forEach(path => {
-    //     if (fs.existsSync(path)) {
-    //       moduleMap[resolve(path)] = true
-    //     }
-    //   })
-    //   return {
-    //     preloadedModules: moduleMap,
-    //     transform: {
-    //       experimentalImportSupport: true,
-    //       inlineRequires: {
-    //         blacklist: moduleMap,
-    //       },
-    //     },
-    //   }
-    // },
-  },
+  // transformer: {
+  //   getTransformOptions: () => {
+  //     const moduleMap = {}
+  //     modulePaths.forEach(path => {
+  //       if (fs.existsSync(path)) {
+  //         moduleMap[resolve(path)] = true
+  //       }
+  //     })
+  //     return {
+  //       preloadedModules: moduleMap,
+  //       transform: {
+  //         experimentalImportSupport: true,
+  //         inlineRequires: {
+  //           blacklist: moduleMap,
+  //         },
+  //       },
+  //     }
+  //   },
+  // },
   projectRoot: ROOT_FOLDER,
 }
 
