07ea81fe821bab8aa0fdc9c524a9edd7f9e6a44e
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2019-12-23 17:58:59 +0100

    appcenter-post-clone.sh

--- packages/mobile/appcenter-post-clone.sh
+++ packages/mobile/appcenter-post-clone.sh
@@ -3,7 +3,7 @@
 set -ex
 
 brew uninstall node@6
-NODE_VERSION=$(cat ../../.nvmrc)
+NODE_VERSION="12.13.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
