a9f3f05b2a319c90fc2847e87b33f16b96bb12a1
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-01-07 14:42:05 +0100

    react-native-xcode.sh

--- packages/mobile/tasks/react-native-xcode.sh
+++ packages/mobile/tasks/react-native-xcode.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/bin/sh -e
+
+set -x
 
 DEST=$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
 BUNDLE_FILE="$DEST/main.jsbundle"
 ENTRY_FILE="packages/mobile/index.js"
 
+echo "🚧 Building JS bundle..."
+
+NODE_ENV=production
+
 ../../../node_modules/react-native/cli.js \
   ram-bundle \
-  --entry-file "$ENTRY_FILE"  \
   --platform ios \
   --dev false \
   --reset-cache \
-  --bundle-output "$BUNDLE_FILE" \
-  --assets-dest "$DEST"
+  --entry-file $ENTRY_FILE \
+  --bundle-output $BUNDLE_FILE \
+  --assets-dest $DEST \
+  --verbose \
+  $EXTRA_PACKAGER_ARGS
+
+echo "✅ Bundle saved in $OUTPUT_DIR"
