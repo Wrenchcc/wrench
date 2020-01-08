cec5cf4e3229e3908641feb98a8df976cbd3b23b
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-01-08 23:12:16 +0100

    Include js bundle size in slack

---
+++ fastlane/utils/slack.rb
@@ -0,0 +1,24 @@
+require './utils/filesize'  
+
+# TODO: Add android file size
+def slack_notification(platform, build_number, version, success)
+  bundle_size = filesize(File.size('../../.builds/main.jsbundle'))
+
+  slack(
+    message: "Wrench (#{platform})",
+    pretext: success ? "Build ##{build_number} succeeded :rocket:" : "Build ##{build_number} just broked :scream:",
+    channel: "#release",
+    success: success,
+    use_webhook_configured_username_and_icon: true,
+    payload: { 
+      "Build" => build_number,
+      "Version" => version,
+      "Build Date" => Time.new.to_s,
+      "Bundle Size" => bundle_size
+    },
+    default_payloads: [:git_branch, :git_author],
+    attachment_properties: { 
+      thumb_url: "https://edge-files.wrench.cc/static/email/logo.jpg?w=60&h=60&dpr=3",
+    }
+  )
+end
