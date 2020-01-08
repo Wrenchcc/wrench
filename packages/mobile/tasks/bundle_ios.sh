7ecf550deda663ae21bad9491516cf6ea3ebc8f2
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-01-08 11:49:51 +0100

    Use sentry from fastlane

--- packages/mobile/tasks/bundle_ios.sh
+++ packages/mobile/tasks/bundle_ios.sh
@@ -2,15 +2,17 @@
 
 set -x
 
+PROJECT_ROOT="../../.."
 DEST=$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
 BUNDLE_FILE="$DEST/main.jsbundle"
+SOURCEMAP_FILE="$PROJECT_ROOT/.builds/ios.bundle.map"
 ENTRY_FILE="packages/mobile/index.js"
 
 echo "🚧 Building JS bundle..."
 
 NODE_ENV=production
 
-../../../node_modules/react-native/cli.js \
+$PROJECT_ROOT/node_modules/react-native/cli.js \
   ram-bundle \
   --platform ios \
   --dev false \
@@ -19,6 +21,8 @@ NODE_ENV=production
   --bundle-output $BUNDLE_FILE \
   --assets-dest $DEST \
   --verbose \
+  --sourcemap-output $SOURCEMAP_FILE \
   $EXTRA_PACKAGER_ARGS
 
-echo "✅ Bundle saved in $OUTPUT_DIR"
+echo "✅ Bundle saved in $DEST"
+echo "✅ Sourcemap saved $SOURCEMAP_FILE"
