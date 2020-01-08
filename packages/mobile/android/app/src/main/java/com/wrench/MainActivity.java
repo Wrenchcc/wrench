c0776bc0096a37b9ae40e144e9a40f09639518e6
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-01-08 10:37:38 +0100

    Update dependencies

--- packages/mobile/android/app/src/main/java/com/wrench/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wrench;
 
 import android.os.Bundle;
+// import android.os.Build;
+// import android.view.WindowManager;
 import androidx.annotation.Nullable;
 
 import com.reactnativenavigation.NavigationActivity;
@@ -11,6 +13,16 @@ public class MainActivity extends NavigationActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
     SplashScreen.show(this);
+
+    // https://dev.to/brunolemos/adding-notch-support-to-your-react-native-android-app-3ci3
+    // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+    //     WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+    //     layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+    //     getWindow().setAttributes(layoutParams);
+    //     getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    //     getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    // }
+
     super.onCreate(savedInstanceState);
   }
 }
--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -35,6 +35,7 @@ import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativeimagemanipulator.ImageManipulatorPackage;
 import com.reactnativemedialibrary.MediaLibraryPackage;
 import com.reactnativedevicelocale.DeviceLocalePackage;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
@@ -89,6 +90,7 @@ public class MainApplication extends NavigationApplication {
         packages.add(new MediaLibraryPackage());
         packages.add(new DeviceLocalePackage());
         packages.add(new RNSentryPackage());
+        packages.add(new SafeAreaContextPackage());
 
         return packages;
     }
