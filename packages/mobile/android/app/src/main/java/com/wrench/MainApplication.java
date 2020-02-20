4272fa92e85a4bc273aa7a552fd17fbe84ced388
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-02-20 16:17:36 +0100

    Fix crash on Android

--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -17,7 +17,6 @@ import com.brentvatne.react.ReactVideoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativemedialibrary.MediaLibraryPackage;
 import com.reactnativedevicelocale.DeviceLocalePackage;
-import io.expo.appearance.RNCAppearancePackage;
 
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
@@ -26,26 +25,6 @@ import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-
-// import com.facebook.reactnative.androidsdk.FBSDKPackage;
-// import cl.json.RNSharePackage;
-// import co.apptailor.googlesignin.RNGoogleSigninPackage;
-// import com.dylanvann.fastimage.FastImageViewPackage;
-// import com.facebook.reactnative.androidsdk.FBSDKPackage;
-// import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-// import com.swmansion.reanimated.ReanimatedPackage;
-// import org.devio.rn.splashscreen.SplashScreenReactPackage;
-// import org.reactnative.camera.RNCameraPackage;
-// import com.imagepicker.ImagePickerPackage;
-// import io.sentry.RNSentryPackage;
-// import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
-// import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-// import com.reactnativecommunity.netinfo.NetInfoPackage;
-// import com.reactnativecommunity.webview.RNCWebViewPackage;
-// import com.reactnativeimagemanipulator.ImageManipulatorPackage;
-// import io.invertase.firebase.RNFirebasePackage;
-
 
 public class MainApplication extends NavigationApplication {
 
@@ -71,25 +50,6 @@ public class MainApplication extends NavigationApplication {
             packages.add(new DeviceLocalePackage());
             packages.add(new LinearGradientPackage());
             packages.add(new ReactVideoPackage());
-            packages.add(new RNCAppearancePackage());
-
-            // packages.add(new AsyncStoragePackage());
-            // packages.add(new FastImageViewPackage());
-            // packages.add(new FBSDKPackage());
-            // packages.add(new NetInfoPackage());
-            // packages.add(new ReactNativeConfigPackage());
-            // packages.add(new ReanimatedPackage());
-            // packages.add(new RNCameraPackage());
-            // packages.add(new RNCWebViewPackage());
-            // packages.add(new RNFirebasePackage());
-            // packages.add(new RNGestureHandlerPackage());
-            // packages.add(new RNGoogleSigninPackage());
-            // packages.add(new RNSharePackage());
-            // packages.add(new SplashScreenReactPackage());
-            // packages.add(new RNPermissionsPackage());
-            // packages.add(new ImagePickerPackage());
-            // packages.add(new ImageManipulatorPackage());
-            // packages.add(new RNSentryPackage());
 
             return packages;
         }
