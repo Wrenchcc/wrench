dd0c0b91dd3ca7be7b569d268d1c5288dcbcde0f
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-04-17 15:33:10 +0200

    Android

---
+++ packages/mobile/android/app/src/main/java/com/wrench/generated/BasePackageList.java
@@ -0,0 +1,22 @@
+package com.wrench.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.camera.CameraPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagemanipulator.ImageManipulatorPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localization.LocalizationPackage(),
+        new expo.modules.medialibrary.MediaLibraryPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
