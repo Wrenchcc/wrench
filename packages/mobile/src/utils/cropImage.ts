e6ea14871edb9f294da6c228f57e64441dbdcde7
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-04-17 13:05:02 +0200

    Pods

--- packages/mobile/src/utils/cropImage.ts
+++ packages/mobile/src/utils/cropImage.ts
@@ -1,25 +1,19 @@
 import * as ImageManipulator from 'expo-image-manipulator'
 import { logError } from 'utils/sentry'
-import { isAndroid } from 'utils/platform'
 
 const MAX_SIZE = 1500
 
 export default async function cropImage({ uri, crop }) {
   try {
-    if (isAndroid) {
-      // TODO: Enable cropping again dosen't work in prod
-      return Promise.resolve({ uri })
-    } else {
-      return ImageManipulator.manipulateAsync(uri, [
-        { crop },
-        {
-          resize: {
-            width: MAX_SIZE,
-            height: MAX_SIZE,
-          },
+    return ImageManipulator.manipulateAsync(uri, [
+      { crop },
+      {
+        resize: {
+          width: MAX_SIZE,
+          height: MAX_SIZE,
         },
-      ])
-    }
+      },
+    ])
   } catch (err) {
     logError(err, { uri, crop })
   }
