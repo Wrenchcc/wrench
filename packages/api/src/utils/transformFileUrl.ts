df2a53044214142a187bc94d688e7c0b0c57ea6e
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-05-26 22:35:18 +0200

    Enable cdn

--- packages/api/src/utils/transformFileUrl.ts
+++ packages/api/src/utils/transformFileUrl.ts
@@ -1,3 +1,5 @@
-const { CDN_DOMAIN } = process.env
+// const { CDN_DOMAIN } = process.env
+
+const CDN_DOMAIN = 'https://cdn.wrench.cc'
 
 export const transformFileUrl = (filename) => `${CDN_DOMAIN}/images/${filename}`
