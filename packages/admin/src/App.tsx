29a021cba21f2e6a04b37208a477a41556180d60
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-06-01 11:09:16 +0200

    Update dependencies

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,22 +1,71 @@
 // @ts-nocheck
 import './styles.css'
-// import React, { useState, useEffect } from 'react'
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import { useCurrentUserLazyQuery } from '@wrench/common'
-// import useLocalStorage from './utils/useLocalStorage'
-// import Header from './components/Header'
-// import Panel from './components/Panel'
-// import Login from './pages/Login'
-// import Dashboard from './pages/Dashboard'
-// import Users from './pages/Users'
-// import Posts from './pages/Posts'
-// import Projects from './pages/Projects'
-// import Comments from './pages/Comments'
-// import Newsletter from './pages/Newsletter'
-// import PushNotifications from './pages/PushNotifications'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useCurrentUserLazyQuery } from '@wrench/common'
+import useLocalStorage from './utils/useLocalStorage'
+import Header from './components/Header'
+import Panel from './components/Panel'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
+import Posts from './pages/Posts'
+import Projects from './pages/Projects'
+import Comments from './pages/Comments'
+import Newsletter from './pages/Newsletter'
+import PushNotifications from './pages/PushNotifications'
 
 function App() {
-  return 'hej'
+  const [accessToken] = useLocalStorage('access_token')
+  const [loadUser, { data, loading }] = useCurrentUserLazyQuery()
+
+  const [isAuthenticated, setAuthenticated] = useState(!!accessToken)
+
+  useEffect(() => {
+    loadUser()
+  }, [isAuthenticated, loadUser])
+
+  if (loading) {
+    return null
+  }
+
+  return (
+    <Router>
+      <Header />
+
+      {isAuthenticated && data?.user?.role === 'ADMIN' ? (
+        <>
+          <Panel setAuthenticated={setAuthenticated} />
+
+          <Switch>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/posts">
+              <Posts />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/comments">
+              <Comments />
+            </Route>
+            <Route path="/newsletter">
+              <Newsletter />
+            </Route>
+            <Route path="/push-notifications">
+              <PushNotifications />
+            </Route>
+          </Switch>
+        </>
+      ) : (
+        <Login setAuthenticated={setAuthenticated} />
+      )}
+    </Router>
+  )
 }
 
 export default App
