87ee8b83fc7508e509055ac34aadfa65fb0b8591
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-06-14 15:23:41 +0200

    wip

--- packages/common/dist/graphql/utils/usePaginatedLazyQuery.d.ts
+++ packages/common/dist/graphql/utils/usePaginatedLazyQuery.d.ts
@@ -1,9 +1,9 @@
 declare const _default: (path: any, initialData?: any) => (query: any, options?: any) => {
     loadData: (options?: import("@apollo/react-hooks").QueryLazyOptions<Record<string, any>> | undefined) => void;
-    error: any;
-    refetch: any;
+    error: import("apollo-client").ApolloError | undefined;
+    refetch: (variables?: Record<string, any> | undefined) => Promise<import("apollo-client").ApolloQueryResult<any>>;
     data: any;
-    fetchMore: (options?: any) => Promise<any>;
+    fetchMore: (options?: any) => Promise<import("apollo-client").ApolloQueryResult<any>>;
     hasNextPage: boolean;
     isFetching: boolean;
     isRefetching: boolean;
