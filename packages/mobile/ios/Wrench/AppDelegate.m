ff1a8f3a092f5d9ca6fa835ba039913bd805773d
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-07-30 12:22:38 +0200

    Fix build

--- packages/mobile/ios/Wrench/AppDelegate.m
+++ packages/mobile/ios/Wrench/AppDelegate.m
@@ -11,35 +11,15 @@
 #import <CodePush/CodePush.h>
 #import <Firebase.h>
 
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
-#endif
-
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  #ifdef FB_SONARKIT_ENABLED
-    InitializeFlipper(application);
-  #endif
-  
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {  
+
   [FIROptions defaultOptions].deepLinkURLScheme = @"wrench";
   if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
   }
+
   [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
 
   // Register WebP format support
