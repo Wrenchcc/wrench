82564f5c13e6eb08c847e547d12c9e8d077617dc
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-08-24 10:08:17 +0200

    Update dependencies

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -4,14 +4,14 @@ import { useDarkMode } from 'storybook-dark-mode'
 import ThemeProvider from '../src/ThemeProvider'
 import { THEMES } from '../src/theme'
 
-addDecorator(renderStory => (
+addDecorator((renderStory) => (
   <ThemeProvider mode={useDarkMode() ? THEMES.DARK : THEMES.LIGHT}>{renderStory()}</ThemeProvider>
 ))
 
 const req = require.context('../src', true, /stories(\.web|\.native)?.tsx$/)
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach((filename) => req(filename))
 }
 
 configure(loadStories, module)
