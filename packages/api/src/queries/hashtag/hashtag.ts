ba6693ff65bcff66bd3616ab5086de264cb28f6b
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-09-29 21:15:23 +0200

    Cache response

--- packages/api/src/queries/hashtag/hashtag.ts
+++ packages/api/src/queries/hashtag/hashtag.ts
@@ -1,7 +1,18 @@
-// TODO: Use dataloader
-export default async (_, args, ctx) =>
-  ctx.db.Hashtag.findOne({
+export default async (_, args, ctx) => {
+  const cacheKey = `hashtag:hashtag:${JSON.stringify(args)}`
+  const cache = await ctx.redis.get(cacheKey)
+
+  if (cache) {
+    return cache
+  }
+
+  const response = await ctx.db.Hashtag.findOne({
     where: {
       ...args,
     },
   })
+
+  ctx.redis.set(cacheKey, response)
+
+  return response
+}
--- packages/api/src/queries/user/interestedIn.ts
+++ packages/api/src/queries/user/interestedIn.ts
@@ -1,6 +1,13 @@
 import { In } from 'typeorm'
 
 export default async ({ id }, _, ctx) => {
+  const cacheKey = `user:interestedIn:${id}`
+  const cache = await ctx.redis.get(cacheKey)
+
+  if (cache) {
+    return cache
+  }
+
   const interestedIn = await ctx.db.UserInterestedIn.find({
     userId: id,
   })
@@ -13,5 +20,9 @@ export default async ({ id }, _, ctx) => {
     },
   })
 
-  return projectTypes.length > 0 ? projectTypes : null
+  const response = projectTypes.length > 0 ? projectTypes : null
+
+  ctx.redis.set(cacheKey, response)
+
+  return response
 }
