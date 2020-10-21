040502d925dee8150f74ce4427ee52e2d4740643
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-10-21 10:23:48 +0200

    Remove cache on collections

--- packages/api/src/mutations/collection/collectPosts.ts
+++ packages/api/src/mutations/collection/collectPosts.ts
@@ -9,7 +9,7 @@ export default isAuthenticated(async (_, { projectId, collectionId, input }, ctx
     return new ForbiddenError('You don’t have permission to manage this project.')
   }
 
-  await ctx.redis.delete(`project:collectionsConnection:${projectId}:*`)
+  // await ctx.redis.delete(`project:collectionsConnection:${projectId}:*`)
 
   input.map(async ({ postId }) => {
     ctx.db.PostCollection.save({
--- packages/api/src/mutations/collection/deleteCollection.ts
+++ packages/api/src/mutations/collection/deleteCollection.ts
@@ -8,8 +8,6 @@ export default isAuthenticated(async (_, { id, projectId }, ctx) => {
     return new ForbiddenError('You don’t have permission to manage this project.')
   }
 
-  const cacheKey1 = `project:collectionsConnection:${projectId}:*`
-  
   const [collection] = await Promise.all([
     // ctx.db.Collection.find(id),
     ctx.db.ProjectCollection.delete({
@@ -20,17 +18,8 @@ export default isAuthenticated(async (_, { id, projectId }, ctx) => {
       collectionId: id,
       projectId,
     }),
-    ctx.redis.delete(cacheKey1),
+    // ctx.redis.delete(cacheKey1),
   ])
-
-  console.log(collection)
-  // const project = await ctx.db.Project.findOne(id)
-
-  // if (!canModerateProject(project, ctx.userId) && !isAdmin(ctx.userId)) {
-  //   return new ForbiddenError('You don’t have permission to manage this project.')
-  // }
-
-  // await ctx.db.Project.delete(id)
 
   return collection
 })
