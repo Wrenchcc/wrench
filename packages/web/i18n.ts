b5a7f77726de149ba07b749b4b89e456f7c813b4
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-10-27 20:28:20 +0100

    Move files

---
+++ packages/web/i18n.ts
@@ -0,0 +1,26 @@
+import NextI18Next from 'next-i18next'
+import * as humanFormat from 'human-format'
+import path from 'path'
+
+export const SUPPORTED_LOCALS = ['en', 'sv']
+
+const NextI18NextInstance = new NextI18Next({
+  defaultLanguage: 'en',
+  otherLanguages: SUPPORTED_LOCALS,
+  localePath: path.resolve('./public/locales'),
+  interpolation: {
+    escapeValue: false,
+    format(value, format) {
+      if (format === 'humanFormat') {
+        return humanFormat(value, {
+          separator: '',
+          decimals: 1,
+        })
+      }
+
+      return value
+    },
+  },
+})
+
+export default NextI18NextInstance
--- packages/web/src/i18n/index.ts
+++ packages/web/src/i18n/index.ts
@@ -1,27 +1,6 @@
-import NextI18Next from 'next-i18next'
-import path from 'path'
-import * as humanFormat from 'human-format'
+import NextI18NextInstance from '../../i18n'
 
 export const SUPPORTED_LOCALS = ['en', 'sv']
-
-const NextI18NextInstance = new NextI18Next({
-  defaultLanguage: 'en',
-  otherLanguages: SUPPORTED_LOCALS,
-  localePath: path.resolve('./public/locales'),
-  interpolation: {
-    escapeValue: false,
-    format(value, format) {
-      if (format === 'humanFormat') {
-        return humanFormat(value, {
-          separator: '',
-          decimals: 1,
-        })
-      }
-
-      return value
-    },
-  },
-})
 
 export default NextI18NextInstance
 
