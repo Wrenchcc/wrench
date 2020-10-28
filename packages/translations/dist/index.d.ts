e494c7938765436c07240c807277cb75bba2bba6
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-10-28 23:44:23 +0100

    Dist

---
+++ packages/translations/dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const defaultLocale = "en";
+export declare const locales: string[];
---
+++ packages/translations/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.locales = exports.defaultLocale = void 0;
+exports.defaultLocale = 'en';
+exports.locales = ['en', 'sv', 'es', 'it', 'de', 'fr', 'nl', 'ru', 'pt', 'tl', 'no'];
+//# sourceMappingURL=index.js.map
