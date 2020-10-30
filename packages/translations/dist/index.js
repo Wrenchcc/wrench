d269699e62a2f6094f80b930a2bc02d29c0a77fc
Pontus Abrahamsson <pontus.abrahamsson@klarna.com>
2020-10-30 16:11:41 +0100

    Add DA

--- packages/translations/dist/index.js
+++ packages/translations/dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.locales = exports.defaultLocale = void 0;
 exports.defaultLocale = 'en';
-exports.locales = ['en', 'sv', 'es', 'it', 'de', 'fr', 'nl', 'ru', 'pt', 'tl', 'no'];
+exports.locales = ['en', 'sv', 'es', 'it', 'de', 'fr', 'nl', 'ru', 'pt', 'tl', 'no', 'da'];
 //# sourceMappingURL=index.js.map
