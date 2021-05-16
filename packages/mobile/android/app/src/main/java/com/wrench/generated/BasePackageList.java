134f13c9e5a19b8513111bd7c786d9eeba7b9773
Pontus Abrahamsson <pontus@lostisland.co>
2021-05-16 13:19:56 +0200

    Fix android build

--- packages/mobile/android/app/src/main/java/com/wrench/generated/BasePackageList.java
+++ packages/mobile/android/app/src/main/java/com/wrench/generated/BasePackageList.java
@@ -14,11 +14,11 @@ public class BasePackageList {
         new expo.modules.haptics.HapticsPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.imagemanipulator.ImageManipulatorPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.localization.LocalizationPackage(),
         new expo.modules.medialibrary.MediaLibraryPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.storereview.StoreReviewPackage()
     );
   }
