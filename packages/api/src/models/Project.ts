c226b37a4757ada983cc11054c06d2f62fa6a3aa
Pontus Abrahamsson <pontus@lostisland.co>
2021-11-02 12:04:42 +0100

    Update Project.ts

--- packages/api/src/models/Project.ts
+++ packages/api/src/models/Project.ts
@@ -76,7 +76,7 @@ export default class Project extends BaseEntity {
       WHERE
         "f_count" IS NOT NULL
         AND "p_count" IS NOT NULL
-        AND "createdAt" > NOW()::timestamp - interval '280 day'
+        AND "createdAt" > NOW()::timestamp - interval '30 day'
       GROUP BY
         p. "id",
         "f_count",
