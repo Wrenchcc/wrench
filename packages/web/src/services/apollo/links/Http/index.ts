eb48e741ec4ec55aca2ee127ee0dbbf66129cd49
Pontus Abrahamsson <pontus@lostisland.co>
2022-01-12 12:00:04 +0100

    Fix endpoint

--- packages/web/src/services/apollo/links/Http/index.ts
+++ packages/web/src/services/apollo/links/Http/index.ts
@@ -2,6 +2,6 @@ import { BatchHttpLink } from '@apollo/link-batch-http'
 import fetch from 'isomorphic-unfetch'
 
 export default new BatchHttpLink({
-  uri: process.env.API_ENDPOINT,
+  uri: process.env.API_ENDPOINT_EDGE,
   fetch,
 })
