214150224fd8b24c3e9e4072cd5629028230e155
Pontus Abrahamsson <pontus@lostisland.co>
2022-01-13 21:55:10 +0100

    Disable cache

--- packages/api/src/queries/project/projectTypes.ts
+++ packages/api/src/queries/project/projectTypes.ts
@@ -1,18 +1,18 @@
 import { VehicleTypes } from '../../models/enums'
 
 export default async (_, { type = VehicleTypes.MOTORCYCLE }, ctx) => {
-  const cacheKey = `project:projectTypes:v2`
-  const cache = await ctx.redis.get(cacheKey)
+  // const cacheKey = `project:projectTypes:v2`
+  // const cache = await ctx.redis.get(cacheKey)
 
-  if (cache) {
-    return cache
-  }
+  // if (cache) {
+  //   return cache
+  // }
 
   const response = await ctx.db.ProjectType.find({
     type,
   })
 
-  ctx.redis.set(cacheKey, response)
+  // ctx.redis.set(cacheKey, response)
 
   return response
 }
