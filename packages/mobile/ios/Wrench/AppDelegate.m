e1e150d3dd1148176aa58f974b49ce116854d0ac
Pontus Abrahamsson <pontus@lostisland.co>
2022-01-18 15:44:59 +0100

    Remove flipper

--- packages/mobile/ios/Wrench/AppDelegate.m
+++ packages/mobile/ios/Wrench/AppDelegate.m
@@ -18,25 +18,6 @@
 #import <RNGoogleSignin/RNGoogleSignin.h>
 #import <SDWebImageWebPCoder/SDImageWebPCoder.h>
 
-// #if DEBUG
-// #import <FlipperKit/FlipperClient.h>
-// #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-// #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-// #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-// #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-// #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-
-// static void InitializeFlipper(UIApplication *application) {
-//   FlipperClient *client = [FlipperClient sharedClient];
-//   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-//   [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-//   [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-//   [client addPlugin:[FlipperKitReactPlugin new]];
-//   [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-//   [client start];
-// }
-// #endif
-
 @interface AppDelegate () <RCTBridgeDelegate>
 
 @property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
@@ -47,10 +28,6 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {  
-  // #if DEBUG
-  //   InitializeFlipper(application);
-  // #endif
-
   [FIROptions defaultOptions].deepLinkURLScheme = @"wrench";
   if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
