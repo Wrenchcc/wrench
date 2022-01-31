c35ac498ba0ebe0bab11c7349655c25bb69dff5b
Pontus Abrahamsson <pontus@lostisland.co>
2022-01-31 23:42:36 +0100

    Use Expo

--- packages/mobile/android/app/src/main/java/com/wrench/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainActivity.java
@@ -1,25 +1,33 @@
 package com.wrench;
 
-import android.os.Bundle;
-import androidx.annotation.Nullable;
-import android.content.Intent;
-import android.content.res.Configuration;
-
 import com.reactnativenavigation.NavigationActivity;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends NavigationActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      SplashScreen.show(this, R.style.SplashScreenTheme, true); 
-    }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-      Intent intent = new Intent("onConfigurationChanged");
-      intent.putExtra("newConfig", newConfig);
-      sendBroadcast(intent);
-    }
 }
+
+// package com.wrench;
+
+// import android.os.Bundle;
+// import androidx.annotation.Nullable;
+// import android.content.Intent;
+// import android.content.res.Configuration;
+
+// import com.reactnativenavigation.NavigationActivity;
+// import org.devio.rn.splashscreen.SplashScreen;
+
+// public class MainActivity extends NavigationActivity {
+//     @Override
+//     protected void onCreate(@Nullable Bundle savedInstanceState) {
+//       super.onCreate(savedInstanceState);
+//       SplashScreen.show(this, R.style.SplashScreenTheme, true); 
+//     }
+
+//     @Override
+//     public void onConfigurationChanged(Configuration newConfig) {
+//       super.onConfigurationChanged(newConfig);
+//       Intent intent = new Intent("onConfigurationChanged");
+//       intent.putExtra("newConfig", newConfig);
+//       sendBroadcast(intent);
+//     }
+// }
--- packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wrench/MainApplication.java
@@ -1,70 +1,69 @@
 package com.wrench;
 
-import com.wrench.generated.BasePackageList;
-
-import android.app.Application;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
-
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.SingletonModule;
-
 import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.facebook.react.ReactPackage;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
+import com.facebook.react.bridge.JSIModuleSpec;
+import com.facebook.react.bridge.JavaScriptContextHolder;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.reactnativemmkv.MmkvModule;
+
+import expo.modules.ApplicationLifecycleDispatcher;
+import android.content.res.Configuration;
+
 public class MainApplication extends NavigationApplication {
 
-    private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
-
-
-    private final ReactNativeHost mReactNativeHost = new NavigationReactNativeHost(this) {
-        @Override
-        protected String getJSMainModuleName() {
-            return "packages/mobile/index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new ReanimatedJSIModulePackage();
-        }
-
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        public List<ReactPackage> getPackages() {
-            ArrayList<ReactPackage> packages = new PackageList(this).getPackages();
-
-            // Add unimodules
-            List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
-                new ModuleRegistryAdapter(mModuleRegistryProvider)
-            );
-            
-            packages.addAll(unimodules);
-
-            return packages;
-        }
-    };
+  private final ReactNativeHost mReactNativeHost = new NavigationReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
 
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    protected List<ReactPackage> getPackages() {
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        List<ReactPackage> packages = new PackageList(this).getPackages();
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        return packages;
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+        return "packages/mobile/index";
+    }
+
+    @Override
+    protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage();
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+  }
+
+  @Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	  super.onConfigurationChanged(newConfig);
+	  ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+	}
 }
